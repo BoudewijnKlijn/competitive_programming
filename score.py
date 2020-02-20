ea7abe47b9b30c738bab84fa9e77f90526a8f342
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-02-20 19:00:59 +0100

    samples files

---
+++ score.py
@@ -0,0 +1,13 @@
+def load_file(filename):
+    with open(filename, 'r') as f:
+        return f.read().strip()
+
+
+def score(filename_in, filename_out):
+    contents_in = load_file(filename_in)
+    contents_out = load_file(filename_out)
+
+    
+
+    pass
+
