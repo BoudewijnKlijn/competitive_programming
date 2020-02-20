85a28ac7ff08eaedbe1633abd4adcc139a49f78e
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-02-20 19:10:48 +0100

    source files

--- score.py
+++ score.py
@@ -1,13 +1,34 @@
+from collections import namedtuple
+
+
 def load_file(filename):
     with open(filename, 'r') as f:
         return f.read().strip()
 
 
-def score(filename_in, filename_out):
+def score(filename_in='sample_in.txt', filename_out='sample_out.txt'):
     contents_in = load_file(filename_in)
     contents_out = load_file(filename_out)
 
+    Settings = namedtuple('Settings', ['B', 'L', 'D'])
+
+    settings = None
+    book_scores = None
     
+    for i, line in enumerate(contents_in.split('\n')):
+        new = line.split(' ')
+        if i == 0:
+            settings = Settings(*new)
+        elif i == 1:
+            book_scores = new
+        else:
+            pass
+            # rides.append(Ride(*new))
 
-    pass
+        print(settings)
 
+    return settings, book_scores
+
+
+if __name__ == '__main__':
+    score()
