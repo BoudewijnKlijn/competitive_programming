8d45dd6811c78c6dd2d8c0e8296b3bb4284356e8
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-02-20 22:05:27 +0100

    prevent nonunique books

--- script.py
+++ script.py
@@ -168,6 +168,33 @@ def strategy_5():
 
     """
 
+    pass
+
+
+def strategy_6(problem):
+    """
+    sort libraries on total value that can be achieved within simulation time
+    sort books on value
+    prevent non-unique books
+    """
+    output = list()
+    max_score_library = calc_total_score_per_library(problem)
+
+    books = set()
+
+    sorted_libraries = [library for _, (_, library) in sorted(zip(max_score_library, enumerate(problem.libraries)), reverse=True)]
+
+    for library_i, library in enumerate(sorted_libraries):
+        output.append((library.id, []))
+        for book_i, book in enumerate(sorted(library.books, key=lambda x: x.score, reverse=True)):
+            if book.id in books:
+                continue
+            output[library_i][1].append(book.id)
+            books.add(book.id)
+        if len(output[library_i][1]) == 0:
+            output[library_i][1].append(book.id)  # to prevent empty lines
+    return output
+
 # check for double book ids!
 
 # TODO / thoughts
@@ -201,8 +228,9 @@ def main():
 
         # output = strategy_0(loaded)  # 10495004
         # output = strategy_1(loaded)  # 10750360
-        output = strategy_4(loaded)  # xxx
-        #output = strategy_2(loaded)  # 15156535
+        # output = strategy_2(loaded)  # 15156535
+        # output = strategy_4(loaded)  # xxx
+        output = strategy_6(loaded)  # xxx
 
         create_submission_file(output, file_out)
 
