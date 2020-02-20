5eb570d34475a45512d3b60d3e96a37826f6526c
Mathijs <mathijs.mul@viqtordavis.com>
2020-02-20 21:49:52 +0100

    add s 3

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import numpy as np
 from collections import namedtuple
 import matplotlib.pyplot as plt
 from time import time
+from collections import Counter
 
 
 class Problem:
@@ -104,21 +105,70 @@ def strategy_2(problem):
     return output
 
 
-def strategy_3():
+def strategy_3(problem):
+    """
+    sort books on value
+    sort libraries on ascending ratio signup time / total shipping time
+    """
+    output = list()
+    time_ratios_library = calc_ratio_signup_shipping_per_library(problem)
+
+    sorted_libraries = [library for _, (_, library) in sorted(zip(time_ratios_library, enumerate(problem.libraries)))]
+
+    for library_i, library in enumerate(sorted_libraries):
+        output.append((library.id, []))
+        for book_i, book in enumerate(sorted(library.books, key=lambda x: x.score, reverse=True)):
+            output[library_i][1].append(book.id)
+    return output
+
+
+def strategy_4(problem):
+    """
+    sort libraries on ascending ratio signup time / total shipping time
+    sort books on value
+    put unique books first
+    """
+
+    # get all books
+    all_books = []
+
+    for library in problem.libraries:
+        library_book_ids = [book.id for book in library.books]
+        all_books.extend(library_book_ids)
+
+    books_counter = Counter(all_books)
+
+    output = list()
+    time_ratios_library = calc_ratio_signup_shipping_per_library(problem)
+
+    sorted_libraries = [library for _, (_, library) in sorted(zip(time_ratios_library, enumerate(problem.libraries)))]
+
+
+    for library_i, library in enumerate(sorted_libraries):
+        output.append((library.id, []))
+        library_books = library.books
+
+        library_unique_books = [book for book in library.books if books_counter[book.id] == 1]
+        library_non_unique_books = [book for book in library_books if not book in library_unique_books]
+
+        for book_i, book in enumerate(sorted(library_unique_books, key=lambda x: x.score, reverse=True)):
+            output[library_i][1].append(book.id)
+
+        for book_i, book in enumerate(sorted(library_non_unique_books, key=lambda x: x.score, reverse=True)):
+            output[library_i][1].append(book.id)
+
+    return output
+
+
+def strategy_5():
     """
     sort libraries on total value that can be achieved within simulation time
     sort books on value
     start with a libraries that have short signup, and slow shipping time
-    """
-
-
-def strategy_4():
-    """
-    sort libraries on total value that can be achieved within simulation time
-    sort books on value
-    start with a libraries that have short signup, and slow shipping time
 
     """
+
+# check for double book ids!
 
 # TODO / thoughts
 # how to deal with uniqueness of books?
@@ -140,26 +190,29 @@ def main():
     files = ['a_example.txt', 'b_read_on.txt', 'c_incunabula.txt', 'd_tough_choices.txt', 'e_so_many_books.txt',
              'f_libraries_of_the_world.txt']
 
-    for file in files:
+    for idx, file in enumerate(files):
 
         file_out = 'out_' + file
 
-        loaded = load_file(file)
+        loaded = load_input_file(file)
         # print(loaded)
         # print(loaded.books)
         # print(loaded.libraries)
 
         # output = strategy_0(loaded)  # 10495004
         # output = strategy_1(loaded)  # 10750360
-        output = strategy_2(loaded)  # xxx
+        output = strategy_4(loaded)  # xxx
 
         create_submission_file(output, file_out)
+
+        # if idx == 0:
+        #     break
 
 
 def analyse():
     file = 'a_example.txt'
     # file = 'b_read_on.txt'
-    loaded = load_file(file)
+    loaded = load_input_file(file)
     max_score_library = calc_total_score_per_library(loaded)
 
     print([j for _, j in sorted(zip(max_score_library, loaded.libraries), reverse=True)])
@@ -181,6 +234,20 @@ def calc_total_score_per_library(problem):
 
     return max_score_library
 
+def calc_ratio_signup_shipping_per_library(problem):
+    D = problem.scanning_days
+    ratio_time_library = list()
+    for library in problem.libraries:
+        # max_possible_scan_time = D - library.signup_time
+        # max_possible_books_to_scan = max_possible_scan_time * library.scanned_daily
+        # sorted_book_scores = sorted([book.score for book in library.books], reverse=True)
+        # max_score_library.append(sum(sorted_book_scores[:max_possible_books_to_scan]))
+        total_shipping_time = library.nr_books / library.scanned_daily
+
+        ratio_signup_shipping = library.signup_time / total_shipping_time
+
+        ratio_time_library.append(ratio_signup_shipping)
+    return ratio_time_library
 
 if __name__ == '__main__':
     main()
