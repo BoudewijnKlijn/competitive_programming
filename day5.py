350baa32ad9d3415651976bbacbe531f127bc394
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-05 06:14:38 +0100

    day4 part1

--- day5.py
+++ day5.py
@@ -4,14 +4,34 @@ import re
 
 def load_data():
     with open(input_file, 'r') as f:
-        data = [x.split() for x in f.read().strip().split('\n\n')]
-
+        data = f.read().strip().split()
     return data
 
 
 def part1(data):
-    pass
+    seat_ids = list()
 
+    for seat in data:
+        # seat = 'FFFBBBFRRR'
+        # find row
+        print(seat)
+        row_number, col_number = 0, 0
+        for i, row_letter in enumerate(seat[:7]):
+            print(row_letter)
+            if row_letter == 'B':
+                row_number += 2 ** (7-i-1)
+        print(row_number)
+
+        for i, col_letter in enumerate(seat[7:]):
+            print(col_letter)
+            if col_letter == 'R':
+                col_number += 2 ** (3-i-1)
+        print(col_number)
+
+        seat_id = row_number * 8 + col_number
+        print(seat_id)
+        seat_ids.append(seat_id)
+    return max(seat_ids)
 
 def part2(data):
     pass
@@ -26,7 +46,7 @@ def main():
 
 
 if __name__ == '__main__':
-    input_file = 'input4.txt'
+    input_file = 'input5.txt'
     data = load_data()
     main()
 
