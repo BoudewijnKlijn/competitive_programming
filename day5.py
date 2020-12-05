5bda5ffeb47258d931d5395e13ff915e72207729
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-05 06:00:52 +0100

    day5 start

---
+++ day5.py
@@ -0,0 +1,35 @@
+import timeit
+import re
+
+
+def load_data():
+    with open(input_file, 'r') as f:
+        data = [x.split() for x in f.read().strip().split('\n\n')]
+
+    return data
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def main():
+    a1 = part1(data)
+    print(a1)
+
+    a2 = part2(data)
+    print(a2)
+
+
+if __name__ == '__main__':
+    input_file = 'input4.txt'
+    data = load_data()
+    main()
+
+    # t = timeit.Timer('par1(data)', globals=globals())
+    # n = 10000
+    # print(sum(t.repeat(repeat=n, number=1)) / n)
