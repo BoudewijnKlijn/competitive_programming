2f90126a1cdfeaab8c4eb454e1528ccaa1230628
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-06 06:00:58 +0100

    day6 start

---
+++ day6.py
@@ -0,0 +1,34 @@
+import timeit
+import re
+
+
+def load_data():
+    with open(input_file, 'r') as f:
+        data = f.read().strip().split()
+    return data
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def main():
+    a1 = part1(data)
+    print(a1)
+
+    a2 = part2(data)
+    print(a2)
+
+
+if __name__ == '__main__':
+    input_file = 'input6.txt'
+    data = load_data()
+    main()
+
+    # t = timeit.Timer('part1(data)', globals=globals())
+    # n = 10000
+    # print(sum(t.repeat(repeat=n, number=1)) / n)
