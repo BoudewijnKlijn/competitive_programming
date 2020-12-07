151f7458907cd8bb159508c2174c0e66a776106c
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-07 08:03:59 +0100

    day7 part2

--- day7.py
+++ day7.py
@@ -14,14 +14,14 @@ def parse_data(data):
         line = line.replace(' bags', '').replace(' bag', '')
         main_bag, contents_part = map(str.strip, line.split('contain'))
         contents = list(map(str.strip, re.findall('[a-z ]{2,}', contents_part)))
+        counts = list(map(int, map(str.strip, re.findall(r'[\d]', contents_part))))
         if len(contents) == 1 and 'no other' in contents[0]:
             contents = list()
-        parsed.update({main_bag: contents})
+        parsed.update({main_bag: dict(zip(contents, counts))})
     return parsed
 
 
-def part1(data):
-    parsed = parse_data(data)
+def part1():
     possible = set()
     needles = ['shiny gold']
     while needles:
@@ -34,21 +34,35 @@ def part1(data):
     return len(possible)
 
 
-def part2(data):
-    pass
+def get_number_of_bags(needle):
+    contents = parsed.get(needle)
+    total = 0
+    for needle, count in contents.items():
+        total += count + count * get_number_of_bags(needle)
+    return total
+
+
+# if len(contents) == 0:
+#     return 1
+# else:
+
+
+def part2():
+    return get_number_of_bags('shiny gold')
 
 
 def main():
-    a1 = part1(data)
+    a1 = part1()
     print(a1)
 
-    a2 = part2(data)
+    a2 = part2()
     print(a2)
 
 
 if __name__ == '__main__':
     input_file = 'input7.txt'
     data = load_data()
+    parsed = parse_data(data)
     main()
 
     # t = timeit.Timer('part1(data)', globals=globals())
