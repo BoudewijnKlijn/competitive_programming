2c4134e8710ab47beecb9934f3da83426cebe3e7
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-09 06:28:38 +0100

    day9 part2

--- day9.py
+++ day9.py
@@ -19,36 +19,39 @@ def is_in_preamble(preamble, pos, goal=2020):
             return n1, n2
 
 
+def contiguous_set(maxi, needle):
+    for begin in range(maxi):
+        for end in range(begin+3, maxi):
+            if needle == sum(data[begin: end]):
+                return min(data[begin: end]) + max(data[begin: end])
+
+
 def part1():
     preamble = 25
     for i, n in enumerate(data):
-        # print(i, n)
         if i < preamble:
             continue
 
-        # print(is_in_preamble(preamble, i, goal=n))
         if not is_in_preamble(preamble, i, goal=n):
-            print(i)
-            exit(n)
+            return n, i
 
 
-
-def part2():
-    pass
+def part2(weakness, maxi):
+    return contiguous_set(maxi, weakness)
 
 
 def main():
-    a1 = part1()
+    a1, i = part1()
     print(a1)
+    # a1, i = 127, 16
 
-    a2 = part2()
+    a2 = part2(a1, maxi=i)
     print(a2)
 
 
 if __name__ == '__main__':
     input_file = 'input9.txt'
     data = load_data()
-    print(data)
     parsed = parse_data()
     main()
 
