ce2ee343d02285f49304e819729576905b019f1f
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-14 07:23:29 +0100

    day14 part2

--- day14.py
+++ day14.py
@@ -26,6 +26,18 @@ def combine_masks(value_mask, memory_mask):
     return memory
 
 
+def combine_masks_p2(value_mask, memory_mask):
+    memory = list(map(str, int_to_mask(0)))
+    for i, (mm, vm) in enumerate(zip(memory_mask, value_mask)):
+        if mm == '0':
+            memory[i] = str(vm)
+        elif mm in '1X':
+            memory[i] = mm
+        else:
+            NotImplementedError()
+    return memory
+
+
 def part1():
     memory_dict = dict()
     for line in data:
@@ -42,9 +54,37 @@ def part1():
     return sum_
 
 
-
 def part2():
-    pass
+    memory_dict = dict()
+    for line in data:
+        if 'mask' in line:
+            memory_mask = line.split(' ')[-1]
+        elif 'mem' in line:
+            index_, value = re.findall(r'[\d]+', line)
+            mem2 = int_to_mask(index_)
+            base = combine_masks_p2(mem2, memory_mask)
+            # print(base)
+            xcount = memory_mask.count('X')
+            # print(xcount)
+            for replacement_run in range(2**xcount):
+                # print(replacement_run)
+                replacement_bits = int_to_mask(replacement_run)[::-1]
+                # print(replacement_bits)
+                trial = base.copy()
+
+                for i, bit in enumerate(trial[::-1]):
+                    if bit == 'X':
+                        trial[-i-1] = str(replacement_bits.pop(0))
+                index_ = int(''.join(trial), 2)
+                # print(index_, value)
+                memory_dict[index_] = int(value)
+                # print('trial', trial)
+
+    sum_ = 0
+    for k, v in memory_dict.items():
+        # print(k, strv)
+        sum_ += v
+    return sum_
 
 
 def main():
