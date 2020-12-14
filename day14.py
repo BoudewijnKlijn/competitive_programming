dc5230ccef03f9ed39cbe5235b361e757f67ba20
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-14 06:37:20 +0100

    day14 part1

--- day14.py
+++ day14.py
@@ -1,12 +1,10 @@
 import timeit
-from itertools import product
-from collections import defaultdict
-import math
+import re
 
 
 def load_data():
     with open(input_file, 'r') as f:
-        data = f.read().strip().split()
+        data = f.read().strip().split('\n')
     return data
 
 
@@ -14,8 +12,35 @@ def parse_data():
     pass
 
 
+def int_to_mask(num):
+    return list(map(int, '{0:0=36b}'.format(int(num))))
+
+
+def combine_masks(value_mask, memory_mask):
+    memory = int_to_mask(0)
+    for i, (mm, vm) in enumerate(zip(memory_mask, value_mask)):
+        if mm != 'X':
+            memory[i] = int(mm)
+        else:
+            memory[i] = int(vm)
+    return memory
+
+
 def part1():
-   pass
+    memory_dict = dict()
+    for line in data:
+        if 'mask' in line:
+            memory_mask = line.split(' ')[-1]
+        elif 'mem' in line:
+            index_, value = re.findall(r'[\d]+', line)
+            mem2 = int_to_mask(value)
+            memory_dict[index_] = combine_masks(mem2, memory_mask)
+
+    sum_ = 0
+    for k, v in memory_dict.items():
+        sum_ += int(''.join(map(str, v)), 2)
+    return sum_
+
 
 
 def part2():
