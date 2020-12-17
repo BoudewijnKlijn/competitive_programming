0951ac36a1fe4ef846f88a34aedd3128663107ce
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-17 10:52:37 +0100

    day17 part2

--- day17.py
+++ day17.py
@@ -10,11 +10,13 @@ def load_data():
 
 def parse_data():
     active_cubes = set()
+    active_hypercubes = set()
     for x, line in enumerate(data):
         for y, symbol in enumerate(line):
             if symbol == '#':
                 active_cubes.add((x, y, 0))
-    return active_cubes
+                active_hypercubes.add((x, y, 0, 0))
+    return active_cubes, active_hypercubes
 
 
 def get_neighbours(cube):
@@ -24,8 +26,16 @@ def get_neighbours(cube):
         range(cube[2] - 1, cube[2] + 2))) - {cube}
 
 
+def get_neighbours_hyper(cube):
+    return set(product(
+        range(cube[0] - 1, cube[0] + 2),
+        range(cube[1] - 1, cube[1] + 2),
+        range(cube[2] - 1, cube[2] + 2),
+        range(cube[3] - 1, cube[3] + 2))) - {cube}
+
+
 def part1():
-    active_cubes = parsed
+    active_cubes, _ = parsed
 
     # min max x, y, z dimensions
     x_min = 0
@@ -56,7 +66,38 @@ def part1():
 
 
 def part2():
-    pass
+    _, active_hypercubes = parsed
+
+    # min max x, y, z dimensions
+    x_min = 0
+    x_max = len(data)
+    y_min = 0
+    y_max = len(data[0])
+    z_min = 0
+    z_max = 1
+    w_min = 0
+    w_max = 1
+
+    for cycle in range(6):
+        active_next_round = set()
+        x_min -= 1
+        x_max += 1
+        y_min -= 1
+        y_max += 1
+        z_min -= 1
+        z_max += 1
+        w_min -= 1
+        w_max += 1
+        for cube in product(range(x_min, x_max), range(y_min, y_max), range(z_min, z_max), range(w_min, w_max)):
+            n_active_around = len(get_neighbours_hyper(cube).intersection(active_hypercubes))
+            if cube in active_hypercubes and n_active_around in [2, 3]:
+                active_next_round.add(cube)
+            elif cube not in active_hypercubes and n_active_around == 3:
+                active_next_round.add(cube)
+
+        active_hypercubes = active_next_round
+
+    return len(active_hypercubes)
 
 
 def main():
