c619584e776a06efbe35256658708b474bf6002d
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-18 22:09:34 +0100

    day18 part1

--- day18.py
+++ day18.py
@@ -12,8 +12,35 @@ def parse_data():
     pass
 
 
+def solve_left_to_right(chars):
+    """Assumes no parentheses"""
+    ans, modifier = None, None
+    for char in chars:
+        if ans is None:
+            assert char not in ['+*'], 'first char expected to be integer'
+            ans = int(char)
+        elif char in list('+*'):
+            modifier = char
+        else:
+            ans = ans * int(char) if modifier == '*' else ans + int(char)
+    return ans
+
+
 def part1():
-    print(data)
+    """Reduce line to line without parentheses first. Find, solve and replace parts between parenthesis"""
+    pattern = re.compile(r'[\d]+|[\*\(\)\+]')
+    total = 0
+    for line in data:
+        groups = pattern.findall(line)
+        while '(' in groups or ')' in groups:
+            parentheses = [(i, char) for i, char in enumerate(groups) if char in list('()')]
+            for (i, parenthesis1), (j, parenthesis2) in zip(parentheses[:-1], parentheses[1:]):
+                if parenthesis1 == '(' and parenthesis2 == ')':
+                    replacement = solve_left_to_right(groups[i+1:j])
+                    groups = groups[:i] + [replacement] + groups[j+1:]
+                    break
+        total += solve_left_to_right(groups)
+    return total
 
 
 def part2():
