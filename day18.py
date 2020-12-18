3e5c51f7a9db1b58d4240121314b264820aa5cf6
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-18 20:24:58 +0100

    day18 start

---
+++ day18.py
@@ -0,0 +1,39 @@
+import timeit
+import re
+
+
+def load_data():
+    with open(input_file, 'r') as f:
+        data = f.read().strip().split('\n')
+    return data
+
+
+def parse_data():
+    pass
+
+
+def part1():
+    print(data)
+
+
+def part2():
+    pass
+
+
+def main():
+    a1 = part1()
+    print(a1)
+
+    a2 = part2()
+    print(a2)
+
+
+if __name__ == '__main__':
+    input_file = 'input18.txt'
+    data = load_data()
+    parsed = parse_data()
+    main()
+
+    # t = timeit.Timer('part1(p1=False)', globals=globals())
+    # n = 100
+    # print(sum(t.repeat(repeat=n, number=1)) / n)
