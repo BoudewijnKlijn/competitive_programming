ffbcc3a27b4f4385cd10fb3aecf06dcd08ee02c0
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-19 13:23:52 +0100

    day 19 part2 (with regex in the end)

--- day19.py
+++ day19.py
@@ -60,31 +60,6 @@ def part1():
 
 
 def part2():
-    """abandoned. runs into memory problems quickly"""
-    parsed_rules, messages = parsed
-
-    pattern = re.compile(r'([\d]+): ([\d]+).?([\d]+)?.?\|?.?([\d]+)?.?([\d]+)?')
-    extra_rules = """8: 42 | 42 8\n11: 42 31 | 42 11 31\n0: 8 11"""
-    rules = extra_rules.split('\n')
-    max_extra_loop = 1
-    for _ in range(max_extra_loop):
-        for rule in rules:
-            m = pattern.search(rule)
-
-            if m.group(3) is not None:
-                parsed_rules[m.group(1)].update([x + y for x, y in
-                                                 product(parsed_rules[m.group(2)], parsed_rules[m.group(3)])])
-            else:
-                parsed_rules[m.group(1)].update(parsed_rules[m.group(2)])
-
-            if m.group(4) is not None and m.group(5) is not None:
-                parsed_rules[m.group(1)].update([x + y for x, y in
-                                                 product(parsed_rules[m.group(4)], parsed_rules[m.group(5)])])
-            elif m.group(4) is not None:
-                parsed_rules[m.group(1)].update(parsed_rules[m.group(4)])
-
-
-def part2_regex():
     """8: 42 | 42 8
     11: 42 31 | 42 11 31
     0: 8 11"""
@@ -92,55 +67,22 @@ def part2_regex():
     parsed_rules, messages = parsed
     valid = 0
     for message in messages.split('\n'):
-        pattern = r'^(' + '|'.join(parsed_rules['42']) + '){2,}(' + '|'.join(parsed_rules['31']) + ')+$'
-        m = re.search(pattern, message)
-        if m is not None:
-            valid += 1
+        for repeat in range(1, 10):
+            pattern = r'^(' + '|'.join(parsed_rules['42']) + ')+(' + '|'.join(parsed_rules['42']) + '){' + \
+                      str(repeat) + '}(' + '|'.join(parsed_rules['31']) + '){' + str(repeat) + '}$'
+            m = re.search(pattern, message)
+            if m is not None:
+                valid += 1
+                break
     return valid
-
-
-# def part2_recursion():
-#     """8: 42 | 42 8
-#         11: 42 31 | 42 11 31
-#         0: 8 11
-#
-#     it is a combination 42 (at least 2) and 31 (at least 1)"""
-#
-#     parsed_rules, messages = parsed
-#     valid = 0
-#     for message in messages.split('\n'):
-#         print('new', message)
-#         print(is_valid(message))
-#     exit()
-
-
-# def is_valid(message):
-#     parsed_rules, _ = parsed
-#
-#     if len(message) == 0:
-#         return True
-#
-#     for parsed_42 in parsed_rules['42']:
-#         if str.startswith(message, parsed_42):
-#             print(message, parsed_42)
-#             print(len(parsed_42))
-#             if is_valid(message[len(parsed_42):]):
-#                 return True
-
-
 
 
 def main():
     a1 = part1()
     print(a1)
 
-    # a2 = part2()
-    # print(a2)
-
-    a2 = part2_regex()
+    a2 = part2()
     print(a2)
-
-
 
 
 if __name__ == '__main__':
@@ -149,6 +91,6 @@ if __name__ == '__main__':
     parsed = parse_data()
     main()
 
-    # t = timeit.Timer('part1(p1=False)', globals=globals())
-    # n = 100
+    # t = timeit.Timer('part2_regex()', globals=globals())
+    # n = 10
     # print(sum(t.repeat(repeat=n, number=1)) / n)
