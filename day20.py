29c6a286f933f7e7d9bf9304d0b712bbabb8e6c0
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-20 07:38:19 +0100

    day20 position of images. now fix orientation

--- day20.py
+++ day20.py
@@ -2,6 +2,7 @@ import timeit
 import re
 from itertools import product
 from collections import defaultdict
+from collections import Counter
 
 
 def load_data():
@@ -34,7 +35,7 @@ def parse_data():
         for edge in edges[name]['normal']:
             edges[name]['reversed'].append(edge[::-1])
 
-    matches = defaultdict(list)
+    matches = defaultdict(set)
     for name, edge_type in edges.items():
         for name2, edge_type2 in edges.items():
             if name == name2:
@@ -43,7 +44,7 @@ def parse_data():
             for edge1, edge2 in product(edge_type['normal'] + edge_type['reversed'],
                                         edge_type2['normal'] + edge_type2['reversed']):
                 if edge1 == edge2:
-                    matches[name].append(name2)
+                    matches[name].add(name2)
 
     return images, edges, matches
 
@@ -52,14 +53,47 @@ def part1():
     images, edges, matches = parsed
 
     ans = 1
+    n_matches = list()
     for name, match in matches.items():
-        if len(match) / 2 == 2:
+        if len(match) == 2:
             ans *= name
+        n_matches.append(len(match))
+    # print(Counter(n_matches))
     return ans
 
 
 def part2():
-    pass
+    images, edges, matches = parsed
+
+    # start building the image with one corner image. ignore orientation of first one, just find adjacent pieces
+    for name, match in matches.items():
+        if len(match) == 2:
+            break
+
+    total_image = defaultdict(dict)
+    for r, c, in product(range(12), range(12)):
+        # print(r, c)
+        if r == 0 and c == 0:
+            total_image[(r, c)]['name'] = name
+            pass  # todo orientation
+            continue
+
+        # find ajacent images in total image
+        dr = [-1, 0, 1, 0]
+        dc = [0, 1, 0, -1]
+        adjacent_coordinates = [(r+drr, c+dcc) for drr, dcc in zip(dr, dc) if 0 <= r+drr <= 11 and 0 <= c+dcc <= 11]
+        images_placed_and_adjacent = [total_image.get((ac_r, ac_c)).get('name') for ac_r, ac_c in adjacent_coordinates if
+                                      total_image.get((ac_r, ac_c))]
+
+        for name, match in matches.items():
+            # print(match)
+            if len(match) == len(adjacent_coordinates) and all([im in match for im in images_placed_and_adjacent]) \
+                    and name not in set([v['name'] for v in total_image.values()]):
+                total_image[(r, c)]['name'] = name
+                pass  # todo orientation
+                break
+
+    assert set([v['name'] for v in total_image.values()]) == set(matches.keys())
 
 
 def main():
