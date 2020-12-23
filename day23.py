59444f5c257aa736a6e556ed7460d323af526b25
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-23 09:00:13 +0100

    day23 part2

--- day23.py
+++ day23.py
@@ -5,8 +5,6 @@ def load_data():
 
 
 def part1():
-    current_cup_idx = 0
-
     cups = list(map(int, data))
     current_label = cups[0]
     for round_ in range(100):
@@ -40,7 +38,53 @@ def part1():
 
 
 def part2():
-    pass
+    cups = list(map(int, data))
+    cups += list(range(10, int(1e6)+1))
+    circle = dict(zip(cups, cups[1:] + cups[:1]))
+    # print(circle)
+    current_label = cups[-1]
+    # rounds = 100
+    rounds = int(1e7)
+    for round_ in range(rounds):
+        if round_ % 1000000 == 0:
+            print(round_)
+
+        current_label = circle[current_label]
+
+        pick_up = list()
+        tmp_label = current_label
+        for _ in range(3):
+            add = circle[tmp_label]
+            tmp_label = add
+            pick_up.append(add)
+        # print(pick_up)
+        circle[current_label] = circle[tmp_label]
+
+        destination_label = current_label - 1
+        while destination_label in pick_up or destination_label < 1:
+            destination_label -= 1
+            if destination_label < 1:
+                destination_label = max(cups)
+
+        # print(destination_label)
+
+        circle[destination_label], circle[pick_up[-1]] = pick_up[0], circle[destination_label]
+
+        new_current_label = circle[current_label]
+        # print('new', new_current_label)
+        # print(circle)
+
+    # next_ = 1
+    # for _ in range(8):
+    #     next_ = circle[next_]
+    #     # print(next_, end='')
+
+    ans = 1
+    next_ = 1
+    for _ in range(2):
+        next_ = circle[next_]
+        ans *= next_
+    return ans
 
 
 def main():
