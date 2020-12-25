763d67d624c2dd1aa4353be94eec1cfc2077e7c3
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-25 08:30:22 +0100

    day25 no part2

--- day25.py
+++ day25.py
@@ -1,16 +1,7 @@
-import timeit
-from itertools import product
-from collections import defaultdict
-
-
 def load_data():
     with open(input_file, 'r') as f:
         data = f.read().strip().split('\n')
     return data
-
-
-def parse_data():
-    pass
 
 
 def transform(loop_size, subject_number=7, value=1):
@@ -46,24 +37,12 @@ def part1():
     return transform(loop_size=key_loop_tuples[1][1], subject_number=key_loop_tuples[0][0])
 
 
-def part2():
-    pass
-
-
 def main():
     a1 = part1()
     print(a1)
-
-    a2 = part2()
-    print(a2)
 
 
 if __name__ == '__main__':
     input_file = 'input25.txt'
     data = load_data()
-    parsed = parse_data()
     main()
-
-    # t = timeit.Timer('part2()', globals=globals())
-    # n = 30
-    # print(sum(t.repeat(repeat=n, number=1)) / n)
