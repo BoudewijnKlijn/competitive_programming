a6e11f659c84e16b125e7daa1ce3fb68aa6f9ac7
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-25 09:06:20 +0100

    faster and cleaner

--- day25.py
+++ day25.py
@@ -1,40 +1,30 @@
+import timeit
+
+
 def load_data():
     with open(input_file, 'r') as f:
         data = f.read().strip().split('\n')
     return data
 
 
-def transform(loop_size, subject_number=7, value=1):
-    for _ in range(loop_size):
-        value *= subject_number
-        value %= 20201227
-    return value
+def transform(loop_size, subject_number=7):
+    return pow(subject_number, loop_size, 20201227)
 
 
-def search(init_loop_size=0, init_value=1, keys=None):
-    loop_size, value = init_loop_size, init_value
+def search(loop_size=0, value=1, keys=None):
     while value not in keys:
         loop_size += 1
-        value *= 7
-        value %= 20201227
+        value = value * 7 % 20201227
     return value, loop_size
 
 
 def part1():
     pub1, pub2 = map(int, data)
-    # pub1, pub2 = 5764801, 17807724 # sample
-
-    key_loop_tuples = list()
 
     value, loop_size = search(keys={pub1, pub2})
-    key_loop_tuples.append((value, loop_size))
-    assert transform(loop_size=loop_size) == value
+    subject_number = pub2 if value == pub1 else pub1
 
-    value, loop_size = search(init_loop_size=loop_size, init_value=value, keys={pub1, pub2} - {value})
-    key_loop_tuples.append((value, loop_size))
-
-    # mix them up
-    return transform(loop_size=key_loop_tuples[1][1], subject_number=key_loop_tuples[0][0])
+    return transform(loop_size=loop_size, subject_number=subject_number)
 
 
 def main():
@@ -46,3 +36,7 @@ if __name__ == '__main__':
     input_file = 'input25.txt'
     data = load_data()
     main()
+
+    # t = timeit.Timer('part1()', globals=globals())
+    # n = 10
+    # print(sum(t.repeat(repeat=n, number=1)) / n)
