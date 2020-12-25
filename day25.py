c0fb5dec416dc4567cad013cba9ad4d25ec33ecc
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2020-12-25 08:24:43 +0100

    day25 part1

--- day25.py
+++ day25.py
@@ -13,8 +13,37 @@ def parse_data():
     pass
 
 
+def transform(loop_size, subject_number=7, value=1):
+    for _ in range(loop_size):
+        value *= subject_number
+        value %= 20201227
+    return value
+
+
+def search(init_loop_size=0, init_value=1, keys=None):
+    loop_size, value = init_loop_size, init_value
+    while value not in keys:
+        loop_size += 1
+        value *= 7
+        value %= 20201227
+    return value, loop_size
+
+
 def part1():
-    pass
+    pub1, pub2 = map(int, data)
+    # pub1, pub2 = 5764801, 17807724 # sample
+
+    key_loop_tuples = list()
+
+    value, loop_size = search(keys={pub1, pub2})
+    key_loop_tuples.append((value, loop_size))
+    assert transform(loop_size=loop_size) == value
+
+    value, loop_size = search(init_loop_size=loop_size, init_value=value, keys={pub1, pub2} - {value})
+    key_loop_tuples.append((value, loop_size))
+
+    # mix them up
+    return transform(loop_size=key_loop_tuples[1][1], subject_number=key_loop_tuples[0][0])
 
 
 def part2():
