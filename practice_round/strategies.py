26c08cc9f8fc2b0d1694cf6bffaee994db4fc372
Nozziel <nozziel.u.shadow@gmail.com>
2021-02-16 21:31:45 +0100

    8000+

--- practice_round/strategies.py
+++ practice_round/strategies.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from assignment import Pizza, Assignment, read_assignment
 from vqd.score import score
 
@@ -36,28 +38,45 @@ def strategy_0(assignment):
     return orders
 
 
-def strategy_1(problem):
+def strategy_1(problem, state):
     """Eerst pizzas naar alle teams van 4. Daarna naar teams van 3, en dan naar teams van 2.
     Sorteer pizzas eerst op aantal ingredienten om."""
 
-    pizzas = sorted(list(range(100)), reverse=True)
+    # assignment.pizzas.sort(key=lambda x: x.n_ingredients, reverse=False)
+
+    np.random.seed(state)
+
+    copy_pizza = assignment.pizzas.copy()
+
+    np.random.shuffle(copy_pizza)
+
+    pizzas = [pizza.id for pizza in copy_pizza]
 
     orders = list()
 
-    for _ in range(problem.T4):
+    teams_4 = assignment.n_teams_four
+
+    while len(pizzas) >= 4 and teams_4 > 0:
         new_order = pizzas[:4]
         pizzas = pizzas[4:]
-        orders.append(new_order)
+        orders.append((4, new_order))
+        teams_4 -= 1
 
-    for _ in range(problem.T3):
+    teams_3 = assignment.n_teams_three
+
+    while len(pizzas) >= 3 and teams_3 > 0:
         new_order = pizzas[:3]
         pizzas = pizzas[3:]
-        orders.append(new_order)
+        orders.append((3, new_order))
+        teams_3 -= 1
 
-    for _ in range(problem.T2):
+    teams_2 = assignment.n_teams_three
+
+    while len(pizzas) >= 2 and teams_2 > 0:
         new_order = pizzas[:2]
         pizzas = pizzas[2:]
-        orders.append(new_order)
+        orders.append((2, new_order))
+        teams_2 -= 1
 
     return orders
 
@@ -65,10 +84,28 @@ def strategy_1(problem):
 if __name__ == '__main__':
     assignment = read_assignment("b_little_bit_of_everything.in")
 
-    print(assignment.n_teams_four)
+    # print(assignment.n_teams_four)
 
-    output = strategy_0(assignment)
-    print(output)
+    scores = []
+
+    for seed in range(1000, 2000):
+        output = strategy_1(assignment, seed)
+        # print(output)
+
+        with open('temp.out', 'w') as file:
+
+            file.write(f'{len(output)}\n')
+
+            for entry in output:
+                file.write(f'{entry[0]} {" ".join([str(x) for x in entry[1]])}\n')
+        the_score = score('b_little_bit_of_everything.in', 'temp.out')
+        scores.append((seed, the_score))
+        print(f"{seed}: {the_score}")
+
+    scores.sort(key=lambda s: s[1], reverse=True)
+    print(f'{scores[0]=}')
+
+    output = strategy_1(assignment, scores[0][0])
 
     with open('temp.out', 'w') as file:
 
@@ -76,5 +113,5 @@ if __name__ == '__main__':
 
         for entry in output:
             file.write(f'{entry[0]} {" ".join([str(x) for x in entry[1]])}\n')
-
-    print(score("b_little_bit_of_everything.in", 'temp.out'))
+    the_score = score('b_little_bit_of_everything.in', 'temp.out')
+    print(f'{the_score=}')
