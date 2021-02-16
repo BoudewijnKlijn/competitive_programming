a2fd316265f67e8fbd9f581e84993a6004bd44df
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2021-02-16 20:00:34 +0100

    sorteer op aantal

--- practice_round/strategies.py
+++ practice_round/strategies.py
@@ -1,4 +1,5 @@
 def strategy_0(problem):
+    """Eerst 4 pizzas naar alle teams van 4. Daarna 3 naar teams van 3, en dan 2 naar teams van 2."""
 
     pizzas = list(range(100))
 
@@ -20,3 +21,30 @@ def strategy_0(problem):
         orders.append(new_order)
 
     return orders
+
+
+def strategy_1(problem):
+    """Eerst pizzas naar alle teams van 4. Daarna naar teams van 3, en dan naar teams van 2.
+    Sorteer pizzas eerst op aantal ingredienten om."""
+
+    pizzas = sorted(list(range(100)), reverse=True)
+
+    orders = list()
+
+    for _ in range(problem.T4):
+        new_order = pizzas[:4]
+        pizzas = pizzas[4:]
+        orders.append(new_order)
+
+    for _ in range(problem.T3):
+        new_order = pizzas[:3]
+        pizzas = pizzas[3:]
+        orders.append(new_order)
+
+    for _ in range(problem.T2):
+        new_order = pizzas[:2]
+        pizzas = pizzas[2:]
+        orders.append(new_order)
+
+    return orders
+
