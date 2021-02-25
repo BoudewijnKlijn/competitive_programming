63eee657f3493de6ee902dbc865b83c66142befa
Nozziel <nozziel.u.shadow@gmail.com>
2021-02-25 19:24:58 +0100

    schedule class

--- qualifier/schedule.py
+++ qualifier/schedule.py
@@ -6,4 +6,4 @@ class Schedule:
     def __str__(self):
         text = f'{self.intersection}\n{self.street_duration_tuples.count()}\n'
         for street in self.street_duration_tuples:
-            text += f'{street[0]} {street[1]}'
+            text += f'{street[0]} {street[1]}\n'
