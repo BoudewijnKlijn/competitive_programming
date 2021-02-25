3b4c1c85cec8e3e8ffaf10cb814763948d55ea41
Mathijs <mathijs.mul@viqtordavis.com>
2021-02-25 21:04:44 +0100

    More scoring

--- qualifier/calculate_score.py
+++ qualifier/calculate_score.py
@@ -1,6 +1,7 @@
 from qualifier.output_data import OutputData
 from qualifier.input_data import InputData, Street, Car, Intersection
 from typing import Dict, List, Optional
+from qualifier.schedule import Schedule
 
 
 class CarLocation:
@@ -21,6 +22,7 @@ class CarLocation:
         self.duration_current_street = duration_current_street
         self.remaining_itinerary = remaining_itinerary
         self.moving = moving
+        self.status = None
 
     def continue_on_street(self):
         self.sec_on_street += 1
@@ -29,6 +31,11 @@ class CarLocation:
         self.current_start_intersection = self.current_end_intersection
         self.current_street = self.remaining_itinerary[0]
         self.remaining_itinerary.pop()
+
+        if len(self.remaining_itinerary) == 0:
+            self.status = "DONE"
+            self.sec_on_street = -1
+
         self.duration_current_street = new_duration
         self.current_end_intersection = new_destination
         self.sec_on_street = 0
@@ -42,19 +49,24 @@ def calculate_score(input: InputData, solution: OutputData) -> int:
     if not validate_output(solution):
         raise ValueError("Not valid output")
 
-    # TODO
     car2location = get_initial_locations(input)
+    score = 0
 
     for moment in range(input.duration):
         for car in input.cars:
             car_location = car2location[car]
-            print(car_location.__dict__)
 
+            # Car is already done
+            if car_location.sec_on_street < 1:
+                score += 1
             if car_location.sec_on_street == car_location.duration_current_street:
-                next_street = input.name2street[car_location.remaining_itinerary[0]]
+                next_street = car_location.remaining_itinerary[0]
                 car_location.update_intersections(
                     new_destination=next_street.end, new_duration=next_street.time
                 )
+
+                if car_location.status == "DONE":
+                    score += input.bonus
 
             # Just drive
             if 0 < car_location.sec_on_street < car_location.duration_current_street:
@@ -69,31 +81,39 @@ def calculate_score(input: InputData, solution: OutputData) -> int:
                     current_intersection
                 ]
 
-                # traffic_light_green = solution.input.name2street[
+                light_green = False
+                cycle_length = sum(t[1] for t in intersection_schedule.street_duration_tuples)
+                modulo_moment = moment % cycle_length
 
-                # traffic_light_status = get_traffic_light(intersection_schedule, moment)
-                #
-                # if traffic_light_green and no_queue:
-                #     car_location.continue_on_street()
-                #
-                # else:
-                #     # Wait
-                #     pass
+                cumulative_schedule = []
+                time = 0
+                for t in intersection_schedule.street_duration_tuples:
+                    time = time + t[1]
+                    cumulative_schedule.append((t[0], time))
+                    if modulo_moment < time:
+                        if t[0] == current_intersection:
+                            light_green = True
+                        else:
+                            break
 
-    return 0
+                if light_green and no_queue:
+                    car_location.continue_on_street()
+
+                else:
+                    # Wait
+                    pass
+
+    return score
 
 
 def get_initial_locations(input: InputData) -> Dict[Car, CarLocation]:
     locations = dict()
     for car in input.cars:
-        print(car.path)
 
-        first_street_name = car.path[0]
-        first_street = input.name2street[first_street_name]
+        first_street = car.path[0]
         start_intersection = first_street.begin
 
-        second_street_name = car.path[1]
-        second_street = input.name2street[second_street_name]
+        second_street = car.path[1]
         end_intersection = second_street.end
         total_street_duration = second_street.time
 
@@ -121,5 +141,12 @@ def get_end_intersection(street):
 sample_input = InputData(
     "/Users/mathijs/Documents/code/2021-google-hash-code/qualifier/inputs/a.txt"
 )
-print(sample_input)
-calculate_score(sample_input, None)
+
+schedules = [
+    Schedule(intersection=1, street_duration_tuples=[("rue-d-athenes", 2)]),
+    Schedule(intersection=0, street_duration_tuples=[("rue-de-londres", 2)]),
+    Schedule(intersection=2, street_duration_tuples=[("rue-de-moscou", 1)])
+]
+sample_output = OutputData(schedules=schedules)
+s = calculate_score(sample_input, sample_output)
+print(s)
