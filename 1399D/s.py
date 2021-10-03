c22bcf7cc74039c736c7d79a07d79e9d4770192e
Boudewijn Klijn <boudewijn.klijn@viqtordavis.com>
2021-10-03 11:53:46 +0200

    1399D correct. time limit exceeded on test 6

--- 1399D/s.py
+++ 1399D/s.py
@@ -15,101 +15,34 @@ if len(sys.argv) > 1:
 def main():
     t = int(input())
     for _ in range(t):
-        _ = int(input())
+        n = int(input())
         s = input().strip()
-        streaks = make_streaks(s)
-        removed_streaks = remove_and_combine(streaks)
-        print(len(removed_streaks))
-        out = [0] * len(s)
-        for i, streak in enumerate(removed_streaks, start=1):
-            for id in streak.ids:
-                out[id] = i
-        print(' '.join(map(str, out)))
 
+        # Init
+        subsequences = [0] * n
+        prev_char_of_subsequence = dict()
+        prev_char = None
+        for i, char in enumerate(s):
+            # Different character. Try subsequences starting from 1.
+            if char != prev_char:
+                subsequence = 1
+            # Same character. Try subsequences starting from last subsequence plus 1.
+            else:
+                subsequence += 1
 
-class Streak2:
-    def __init__(self, id, ids, chars, before, after):
-        self.id = id
-        self.ids = ids
-        self.chars = chars
-        self.length = len(chars)
-        self.before = before
-        self.after = after
+            # If subsequence already used, the last character in that subsequence has to be different from
+            # the current character. Otherwise, increase subsequence to try that one.
+            while prev_char_of_subsequence.get(subsequence) == char:
+                subsequence += 1
 
-    def __repr__(self):
-        return f'{self.ids=}, {self.chars=}, {self.length=}'
+            # Store values and prepare for next char.
+            subsequences[i] = subsequence
+            prev_char_of_subsequence[subsequence] = char
+            prev_char = char
 
-
-def make_streaks(s):
-    this_id = 0
-    before_id = -1
-    after_id = 1
-    streaks = dict()
-    streak_ids = [0]
-    streak_values = [s[0]]
-    for i, char in enumerate(s[1:], start=1):
-        same_char = char == streak_values[-1]
-        if not same_char:
-            streak_values.append(char)
-            streak_ids.append(i)
-            continue
-
-        streaks[this_id] = Streak2(id=this_id, ids=streak_ids, chars=streak_values, before=before_id, after=after_id)
-        streak_ids = [i]
-        streak_values = [char]
-        this_id += 1
-        before_id += 1
-        after_id += 1
-
-    # end of sequence append
-    streaks[this_id] = Streak2(id=this_id, ids=streak_ids, chars=streak_values, before=before_id, after=after_id)
-
-    return streaks
-
-
-def remove_and_combine(streaks):
-    # removing streaks with an even length will combine the ones on the sides.
-    removed_streaks = list()
-    removed = True
-    while removed:
-        removed = False
-        for i, streak in streaks.items():
-            if streak.length % 2 == 0 and streaks.get(streak.before) is not None and streaks.get(streak.after):
-                removed_streaks.append(streak)
-                del streaks[i]
-                removed = True
-
-                # combine
-                streak_before = streaks.get(streak.before)
-                streak_after = streaks.get(streak.after)
-                # if streak_before is not None and streak_after is not None:
-                # Combine before and after streak.
-                streaks[streak_before.id] = Streak2(
-                    id=streak_before.id,
-                    ids=streak_before.ids + streak_after.ids,
-                    chars=streak_before.chars + streak_after.chars,
-                    before=streak_before.before,
-                    after=streak_after.after,
-                )
-                del streaks[streak_after.id]
-
-                # Update the streak after streak after.
-                streak_after_after = streaks.get(streak_after.after)
-                if streak_after_after is not None:
-                    streaks[streak_after_after.id] = Streak2(
-                        id=streak_after_after.id,
-                        ids=streak_after_after.ids,
-                        chars=streak_after_after.chars,
-                        before=streak_before.id,  # only update which streak is before
-                        after=streak_after_after.after,
-                    )
-
-                break
-
-    for _, streak in streaks.items():
-        removed_streaks.append(streak)
-
-    return removed_streaks
+        # Return answer.
+        print(max(subsequences))
+        print(' '.join(map(str, subsequences)))
 
 
 if __name__ == '__main__':
