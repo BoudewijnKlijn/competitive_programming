f599452150623d69e39001842ad6357027f9d79e
sebastianpdw <sebastianpanmandewit@gmail.com>
2021-12-01 22:15:51 +0100

    Implemented BruteForceStrategy

---
+++ HC_2019_Qualification/strategies/bruteforce_solver.py
@@ -0,0 +1,51 @@
+from random import Random
+import time
+
+from HC_2019_Qualification.input_data_2019_q import Pictures
+from HC_2019_Qualification.picture import Orientation
+from HC_2019_Qualification.scorer_2019_q import Scorer2019Q
+from HC_2019_Qualification.slide import Slide
+from HC_2019_Qualification.slides import Slides
+from valcon.strategy import Strategy
+
+
+class BruteForceStrategy(Strategy):
+    def __init__(self, scorer: Scorer2019Q, start_seed=1, max_attempts=1000):
+        self.scorer = scorer
+        self.start_seed = start_seed
+        self.max_attempts = max_attempts
+        self.current_attempt = 0
+
+    def _attempt_randomly(self, input_data: Pictures) -> (Slides, int):
+        slides = []
+        rng = Random(self.start_seed + self.current_attempt)
+        vertical_picture = None
+
+        for picture in input_data.pictures:
+            if picture.orientation == Orientation.HORIZONTAL:
+                slides.append(Slide([picture]))
+            elif vertical_picture:
+                slides.append(Slide([vertical_picture, picture]))
+                vertical_picture = None
+            else:
+                vertical_picture = picture
+
+        rng.shuffle(slides)
+        solution = Slides(slides)
+        scorer = Scorer2019Q(input_data)
+        return solution, scorer.calculate(solution)
+
+    def solve(self, input_data: Pictures) -> Slides:
+        start_time = time.time()
+        best_solution_score = 0
+        best_solution_slides = None
+        for i in range(0, self.max_attempts):
+            current_solution, current_score = self._attempt_randomly(input_data)
+            if current_score > best_solution_score:
+                best_solution_score = current_score
+                best_solution_slides = current_solution
+                print(f"Improved model at iteration: {i}, current best score: {best_solution_score}")
+            self.current_attempt += 1
+        elapsed_time = time.time() - start_time
+        print(f"Finished {self.max_attempts} attempts at brute forcing solution in {elapsed_time:.2f} seconds")
+        return best_solution_slides
--- workspace_sebastian.py
+++ workspace_sebastian.py
@@ -1,19 +1,20 @@
 import os
 
-from HC_2019_Qualification.strategies.SortByNTags_solver import SortByNPicturesStrategy
 from HC_2019_Qualification.input_data_2019_q import Pictures
 from HC_2019_Qualification.scorer_2019_q import Scorer2019Q
+from HC_2019_Qualification.strategies.bruteforce_solver import BruteForceStrategy
 
 THIS_PATH = os.path.abspath(os.path.dirname(__file__))
 
 if __name__ == '__main__':
     directory = os.path.join(THIS_PATH, 'HC_2019_Qualification', 'input')
-    input_data = Pictures(os.path.join(directory, 'a_example.txt'))
-
-    strategy = SortByNPicturesStrategy()
-    solution = strategy.solve(input_data)
+    input_data = Pictures(os.path.join(directory, 'b_lovely_landscapes.txt'))
 
     scorer = Scorer2019Q(input_data)
+    strategy = BruteForceStrategy(scorer)
+    solution = strategy.solve(input_data)
+
+    # fix double calculating score
     score = scorer.calculate(solution)
 
     print(f'Score: {score}')
