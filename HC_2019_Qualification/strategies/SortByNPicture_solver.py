958ce1d21bad54e56e1d6ffcaffb426bd8c45baa
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-01 21:07:56 +0100

    made code simpler

--- HC_2019_Qualification/strategies/SortByNPicture_solver.py
+++ HC_2019_Qualification/strategies/SortByNPicture_solver.py
@@ -1,26 +1,12 @@
 from HC_2019_Qualification.input_data_2019_q import Pictures
-from HC_2019_Qualification.picture import Orientation
-from HC_2019_Qualification.slide import Slide
 from HC_2019_Qualification.slides import Slides
+from HC_2019_Qualification.strategies.baseline_solver import BaseLineStrategy
 from valcon.strategy import Strategy
 
 
 class SortByNPicturesStrategy(Strategy):
     def solve(self, input_data: Pictures) -> Slides:
 
-        slides = []
-
-        vertical_picture = None
-
         input_data.pictures = sorted(input_data.pictures, key=lambda x: x.number_of_tags, reverse=True)
 
-        for picture in input_data.pictures:
-            if picture.orientation == Orientation.HORIZONTAL:
-                slides.append(Slide([picture]))
-            elif vertical_picture:
-                slides.append(Slide([vertical_picture, picture]))
-                vertical_picture = None
-            else:
-                vertical_picture = picture
-
-        return Slides(slides)
+        return BaseLineStrategy().solve(input_data)
