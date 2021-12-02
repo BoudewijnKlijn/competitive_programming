4da7ba5f55e17106a8bb00513e3532f66d1a2330
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-02 06:18:44 +0100

    day 2 cleaned

--- adventofcode/2021/d2.py
+++ adventofcode/2021/d2.py
@@ -1,40 +1,40 @@
-from typing import Iterable, List, Union
 import re
+from typing import List, Tuple
 
 
-def load_data(filename) -> List[int]:
+def load_data(filename) -> List[Tuple[str, int]]:
     with open(filename, 'r') as f:
         lines = f.read().strip()
         pattern = re.compile(r'([a-z]+) (\d+)')
         return re.findall(pattern, lines)
 
 
-if __name__ == '__main__':
-    input_file = 'day2.txt'
-    data = load_data(input_file)
+def part1(input_data: List[Tuple[str, int]]) -> int:
+    h = 0
+    v = 0
+
+    for direction, units in input_data:
+        units = int(units)
+        if direction == 'forward':
+            h += units
+        elif direction == 'down':
+            v += units
+        elif direction == 'up':
+            v -= units
+        else:
+            raise NotImplementedError(f'{direction} is not implemented')
+
+    return h * v
 
 
+def part2(input_data: List[Tuple[str, int]]) -> int:
     h = 0
     v = 0
     aim = 0
 
-    # part 1
-    # for direction, units in data:
-    #     units = int(units)
-    #     print(direction, units)
-    #     if direction in ['forward']:
-    #         h += units
-    #     elif direction == 'down':
-    #         v += units
-    #     elif direction == 'up':
-    #         v -= units
-    #     else:
-    #         raise NotImplementedError(f'{direction} is not implemented')
-
-    for direction, units in data:
+    for direction, units in input_data:
         units = int(units)
-        print(direction, units)
-        if direction in ['forward']:
+        if direction == 'forward':
             h += units
             v += aim * units
         elif direction == 'down':
@@ -44,6 +44,12 @@ if __name__ == '__main__':
         else:
             raise NotImplementedError(f'{direction} is not implemented')
 
-    print(f'horizontal: {h} vertical: {v}')
-    print(f'answer: {h * v}')
+    return h * v
 
+
+if __name__ == '__main__':
+    input_file = 'day2.txt'
+    data = load_data(input_file)
+
+    print(f'Part 1: {part1(data)}')
+    print(f'Part 2: {part2(data)}')
