fa79f7a2f2f1e59a09f2998728177e7d59a34b0c
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-05 02:37:42 +0100

    day 3 cleaned

--- adventofcode/2021/d3.py
+++ adventofcode/2021/d3.py
@@ -1,68 +1,100 @@
-import re
-from typing import List, Tuple
+from functools import reduce
+from typing import List, Union
 
 
-def load_data(filename) -> List[str]:
+def load_data(filename) -> str:
     with open(filename, 'r') as f:
-        return f.read().strip().split('\n')
+        return f.read()
 
 
-def get_oxygen_row(columns: List[str]) -> int:
-    rows_to_check = set(range(len(columns[0])))
+def parse_data(content: str) -> List[str]:
+    return content.strip().split('\n')
+
+
+def get_number(name: str) -> int:
+    return int(get_binary(name), 2)
+
+
+def get_binary(name: str) -> str:
+    row_ids_to_check = set(range(len(columns[0])))
+    binary = ''
     for col in columns:
-        col_subset = [char for row_i, char in enumerate(col) if row_i in rows_to_check]
-        counts_0, counts_1 = [col_subset.count(s) for s in ['0', '1']]
+        if name in ['oxygen', 'scrubber']:
+            # Use part of col. Not all rows have to be checked.
+            part_of_col = [char for row_id, char in enumerate(col) if row_id in row_ids_to_check]
 
-        # determine starting bit
-        if counts_1 >= counts_0:
-            starting_bit = '1'
+            # Count the number of zeros and ones.
+            zero_count, one_count = get_count(part_of_col)
         else:
-            starting_bit = '0'
-        print(f'{starting_bit=}')
+            # Count the number of zeros and ones.
+            zero_count, one_count = get_count(col)
 
-        # keep columns that start with the starting bit
-        rows_to_remove = {row_i for row_i, char in enumerate(col) if char != starting_bit}
-        rows_to_check -= rows_to_remove
+        # Determine starting bit.
+        bit = get_bit(name, zero_count, one_count)
+        binary += bit
 
-        if len(rows_to_check) == 1:
-            for row_i in rows_to_check:
-                return row_i
+        if name in ['oxygen', 'scrubber']:
+            # Keep columns that start with the starting bit.
+            row_ids_to_remove = {row_id for row_id, char in enumerate(col) if char != bit}
+            row_ids_to_check -= row_ids_to_remove
+
+            # Stop early if only one row left.
+            if len(row_ids_to_check) == 1:
+                row_id = reduce(int, row_ids_to_check)
+                return data[row_id]
+    return binary
 
 
-def get_scrubber_rating(columns: List[str]) -> int:
-    print(columns)
-    rows_to_check = set(range(len(columns[0])))
-    print()
-    for col in columns:
-        print(col)
-        col_subset = [char for row_i, char in enumerate(col) if row_i in rows_to_check]
-        print(col_subset)
-        counts_0, counts_1 = [col_subset.count(s) for s in ['0', '1']]
-
-        print(counts_0, counts_1)
-
-        # determine starting bit
-        if counts_0 <= counts_1:
-            starting_bit = '0'
+def get_bit(name: str, zero_count: int, one_count: int) -> str:
+    """Get bit or starting bit."""
+    if name == 'gamma':
+        if zero_count >= one_count:
+            return '0'
         else:
-            starting_bit = '1'
-        print(f'{starting_bit=}')
+            return '1'
 
-        # keep columns that start with the starting bit
-        rows_to_remove = {row_i for row_i, char in enumerate(col) if char != starting_bit}
-        print(rows_to_remove)
-        rows_to_check = rows_to_check - rows_to_remove
-        print(rows_to_check)
+    elif name == 'epsilon':
+        if zero_count <= one_count:
+            return '0'
+        else:
+            return '1'
 
-        if len(rows_to_check) == 1:
-            for row_i in rows_to_check:
-                return row_i
+    elif name == 'oxygen':
+        if one_count >= zero_count:
+            return '1'
+        else:
+            return '0'
+
+    elif name == 'scrubber':
+        if zero_count <= one_count:
+            return '0'
+        else:
+            return '1'
+
+    else:
+        raise ValueError(f'Unknown name: {name}')
+
+
+def get_count(chars: Union[str, List[str]]) -> List[int]:
+    """Count the number of zeros and ones in cols."""
+    return [chars.count(char) for char in ['0', '1']]
+
+
+def part1():
+    return get_number('gamma') * get_number('epsilon')
+
+
+def part2():
+    return get_number('oxygen') * get_number('scrubber')
+    # return get_number('scrubber')
 
 
 if __name__ == '__main__':
-    input_file = 'input.txt'
+    # Real data
+    input_file = 'day3.txt'
     data = load_data(input_file)
 
+#     # Sample data
 #     data = """00100
 # 11110
 # 10110
@@ -74,50 +106,13 @@ if __name__ == '__main__':
 # 10000
 # 11001
 # 00010
-# 01010""".strip().split('\n')
+# 01010"""
 
-    print(data)
+    # Parse input data
+    data = parse_data(data)
 
-    # Part 1
-    verticals = list(map(''.join, (zip(*data))))
-    print(verticals)
+    # Input data can be seen as rows. Determine columns.
+    columns = list(map(''.join, (zip(*data))))
 
-    # binary = ''
-    # for col in verticals:
-    #     zero_count = col.count('0')
-    #     one_count = col.count('1')
-    #     if zero_count > one_count:
-    #         binary += '0'
-    #     else:
-    #         binary += '1'
-    #
-    # gamma = int(int(binary, 2))
-    #
-    # binary = ''
-    # for col in verticals:
-    #     zero_count = col.count('0')
-    #     one_count = col.count('1')
-    #     if zero_count < one_count:
-    #         binary += '0'
-    #     else:
-    #         binary += '1'
-    #
-    # epsilon = int(int(binary, 2))
-    #
-    # print(gamma * epsilon)
-
-
-    # Part 2
-
-    oxygen_row = get_oxygen_row(verticals)
-    print(oxygen_row)
-    oxygen = int(data[oxygen_row], 2)
-    print(oxygen)
-
-    print(verticals)
-
-    scrubber_rating_row = get_scrubber_rating(verticals)
-    scrubber_rating = int(data[scrubber_rating_row], 2)
-    print(scrubber_rating)
-
-    print(oxygen * scrubber_rating)
+    print("Part 1:", part1())
+    print("Part 2:", part2())
