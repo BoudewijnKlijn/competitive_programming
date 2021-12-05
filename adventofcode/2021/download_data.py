1b85e1780b6d38f1aa929b9717c64653e85dc74b
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-05 10:42:31 +0100

    added env file with aoc cookie

--- adventofcode/2021/download_data.py
+++ adventofcode/2021/download_data.py
@@ -1,4 +1,7 @@
 from aocd import get_data
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 # Get most recent data. Let function determine what that is.
 data = get_data()
