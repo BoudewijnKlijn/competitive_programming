a1c70a091814fb2ddb2614702f6e94cf4b7d2d07
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-06 06:40:46 +0100

    day 6 cleaned

--- adventofcode/2021/d6.py
+++ adventofcode/2021/d6.py
@@ -11,44 +11,43 @@ def load_data(filename: str) -> str:
         return f.read()
 
 
-def parse_data() -> List[int]:
-    return list(map(int, RAW.strip().split(',')))
+def parse_data(raw_data: str) -> List[int]:
+    return list(map(int, raw_data.strip().split(',')))
 
 
-def solve(days=18) -> int:
+def evolve_one_day(old_dict):
     new_dict = Counter()
-    old_dict = Counter(data)
-    for i in range(days):
-        new_dict = Counter()
-        for k, v in old_dict.items():
-            if k == 0:
-                new_dict[8] = v
-                new_dict[6] += v
-                continue
+    for k, v in old_dict.items():
+        if k == 0:
+            new_dict[8] = v
+            new_dict[6] += v
+            continue
 
-            new_dict[k-1] += v
-        old_dict = new_dict
-    print(new_dict)
-    print(sum(new_dict.values()))
-    return sum(new_dict.values())
+        new_dict[k - 1] += v
+    return new_dict
+
+
+def evolve_multiple_days(days) -> int:
+    """Evolve for a number of days."""
+    old_dict = Counter(data)  # initial state
+    for _ in range(days):
+        old_dict = evolve_one_day(old_dict)
+    return sum(old_dict.values())
 
 
 if __name__ == '__main__':
-    RAW = load_data('input.txt')
-
     # Sample data
-    # RAW = """3,4,3,1,2"""
+    RAW = """3,4,3,1,2"""
+    data = parse_data(RAW)
+    assert evolve_multiple_days(18) == 26
+    assert evolve_multiple_days(80) == 5934
 
-    # Parse data
-    data = parse_data()
+    # Actual data
+    RAW = load_data('day6.txt')
+    data = parse_data(RAW)
 
-    print(data)
+    # Part 1
+    print(f'Part 1: {evolve_multiple_days(80)}')
 
-    # part 1
-    # for days in range(1, 100):
-    #     print(days, solve(days))
-
-    # assert solve(18) == 26
-    # assert solve(80) == 5934
-
-    print(solve(256))
+    # Part 2
+    print(f'Part 2: {evolve_multiple_days(256)}')
---
+++ adventofcode/2021/d7.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+import re
+from typing import List, Tuple, Union
+from collections import defaultdict
+from itertools import cycle
+from collections import Counter
+
+
+def load_data(filename: str) -> str:
+    with open(filename, 'r') as f:
+        return f.read()
+
+
+def parse_data(raw_data: str) -> List[int]:
+    return list(map(int, raw_data.strip().split(',')))
+
+
+if __name__ == '__main__':
+    # Sample data
+    RAW = """3,4,3,1,2"""
+    data = parse_data(RAW)
+
+    # Assert solution is correct
+
+    # Actual data
+    # RAW = load_data('input.txt')
+    # data = parse_data(RAW)
+
+    # Part 1
+
+    # Part 2
