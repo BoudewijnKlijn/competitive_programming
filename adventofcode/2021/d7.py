b860db7873215a2afc7fd39025cd1d4f352cfcac
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-07 06:14:51 +0100

    day 7 uncleaned

--- adventofcode/2021/d7.py
+++ adventofcode/2021/d7.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 from itertools import cycle
 from collections import Counter
 
+import numpy as np
+
 
 def load_data(filename: str) -> str:
     with open(filename, 'r') as f:
@@ -17,15 +19,26 @@ def parse_data(raw_data: str) -> List[int]:
 
 if __name__ == '__main__':
     # Sample data
-    RAW = """3,4,3,1,2"""
+    RAW = """16,1,2,0,4,2,7,1,2,14"""
     data = parse_data(RAW)
 
     # Assert solution is correct
 
     # Actual data
-    # RAW = load_data('input.txt')
-    # data = parse_data(RAW)
+    RAW = load_data('input.txt')
+    data = parse_data(RAW)
 
     # Part 1
+    def triangle(x):
+        return sum(range(0, abs(x)+1))
+
+    best_score = None
+    for trial in range(max(data)):
+        score = sum(map(lambda x: triangle(x), (np.array(data) - trial * np.ones_like(data))))
+        if best_score is None or score < best_score:
+            best_score = score
+            best_i = trial
+
+    print(best_score)
 
     # Part 2
