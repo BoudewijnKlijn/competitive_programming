2fc256745a5540457ffef61a37f8c396fc22633e
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-09 21:03:34 +0100

    day 9 cleaned

--- adventofcode/2021/d9.py
+++ adventofcode/2021/d9.py
@@ -17,78 +17,71 @@ def parse_data(raw_data: str) -> List[str]:
     return raw_data.strip().split('\n')
 
 
-def part1(data: List[str]) -> int:
+def get_values():
+    """x and y are not the best naming choice. I had better used r and c."""
     values = dict()
-
     for x, line in enumerate(data):
         for y, digit in enumerate(line):
             values[(x, y)] = int(digit)
+    return values
 
+
+def get_neighbors():
+    """Make dict with set of neighbor coordinates."""
+    neighbors = defaultdict(set)
+    DX = [1, -1, 0, 0]
+    DY = [0, 0, 1, -1]
+    for x, y in values.keys():
+        for dx, dy in zip(DX, DY):
+            if (x + dx, y + dy) in values.keys():
+                neighbors[(x, y)].add((x + dx, y + dy))
+    return neighbors
+
+
+def part1():
+    """Find points that are lower than all their numbers.
+    Return the sum of (value + 1)."""
     ans = 0
-    # Loop over all x, y values
     for (x, y), value in values.items():
-        # Get neighboring values
-        neighbors = [values.get((x + 1, y), 10), values.get((x - 1, y), 10), values.get((x, y + 1), 10),
-                     values.get((x, y - 1), 10)]
-
-        # count how many times values is lower than neighbors
-        if all([value < n for n in neighbors]):
+        # check if value of this coordinate is smaller than the value of all neighbors
+        if all([value < values.get((xn, yn)) for (xn, yn) in neighbors.get((x, y))]):
             ans += 1 + value
-
     return ans
 
 
-def get_basins(data: List[str]) -> List[List[int]]:
+def should_be_in_basin(a, b):
+    """Only values smaller than 9 should be in a basin."""
+    if values[(a, b)] == 9:
+        return False
+    return True
+
+
+def get_basins():
     basins = list()
-    values = dict()
-
-    n_cols = len(data[0])
-    n_rows = len(data)
-
-    for x, line in enumerate(data):
-        for y, digit in enumerate(line):
-            values[(x, y)] = int(digit)
-
-    neighbor_dict = dict()
-
-    # Loop over all x, y values
-    for (x, y), value in values.items():
-
-        # Get neighbors coordinates up , down, left, right
-        neighbors = set()
-        for dx, dy in product([-1, 0, 1], repeat=2):
-            if (dx == 0 and dy == 0 or abs(dx) == 1 and abs(dy) == 1) or values.get((x+dx, y+dy)) is None:
-                continue
-            neighbors.add((x + dx, y + dy))
-        neighbor_dict[(x, y)] = neighbors
-
-    def should_be_in_basin(a, b):
-        if values[(a, b)] == 9:
-            return False
-        else:
-            return True
-
     checked = set()
-    # Create new basin. Start with the first value. If not 9 it is part of a basin. If already checked. skip.
-    basins = list()
+
+    # One other loop that loops over all the whole grid. It can skip over all points that are already checked.
     for (x, y), value in values.items():
         if (x, y) in checked:
             continue
-
         checked.add((x, y))
 
         neighbors_to_check = set()
         basin = set()
         if should_be_in_basin(x, y):
             basin.add((x, y))
-            neighbors_to_check = neighbors_to_check.union(neighbor_dict[(x, y)]) - checked
+            # Add neighbors to the list of neighbors to check, but remove neighbors that are already checked.
+            neighbors_to_check = neighbors_to_check.union(neighbors[(x, y)]) - checked
 
+        # Loop over the direct neighbors of point in outer loop. Remove and check direct neighbors and add indirect
+        # neighbors to the set.
         while neighbors_to_check:
             (xn, yn) = neighbors_to_check.pop()
             checked.add((xn, yn))
             if should_be_in_basin(xn, yn):
                 basin.add((xn, yn))
-                neighbors_to_check = neighbors_to_check.union(neighbor_dict[(xn, yn)]) - checked
+                # Add neighbors to the list of neighbors to check, but remove neighbors that are already checked.
+                neighbors_to_check = neighbors_to_check.union(neighbors[(xn, yn)]) - checked
 
         basins.append(basin)
 
@@ -104,18 +97,27 @@ if __name__ == '__main__':
 9899965678"""
     data = parse_data(RAW)
 
+    # parse the grid. key=coordinate, value=digit
+    values = get_values()
+
+    # get coordinates of neighbors of each coordinate
+    neighbors = get_neighbors()
+
     # Assert solution is correct
-    assert part1(data) == 15
+    assert part1() == 15
 
     # Actual data
-    RAW = load_data('input.txt')
+    RAW = load_data('day9.txt')
     data = parse_data(RAW)
 
-    print(part1(data))
+    values = get_values()
+    neighbors = get_neighbors()
 
-    basins = get_basins(data)
+    # Part 1
+    print(f"Part 1: {part1()}")
+
+    # Part 2
+    basins = get_basins()
     basin_lengths = sorted(map(len, basins), reverse=True)
-    ans = math.prod(basin_lengths[:3])
-    print(ans)
-
-
+    p2 = math.prod(basin_lengths[:3])
+    print(f"Part 2: {p2}")
