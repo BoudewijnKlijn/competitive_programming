ef65397fe67280cc371fce97a4a45b15903c5e29
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-12 13:03:34 +0100

    day 12 cleaned

--- adventofcode/2021/d12.py
+++ adventofcode/2021/d12.py
@@ -7,6 +7,7 @@ from collections import Counter
 import numpy as np
 from functools import reduce
 from queue import Queue
+from copy import deepcopy
 
 
 def load_data(filename: str) -> str:
@@ -40,46 +41,55 @@ END_CAVE = 'end'
 
 
 def part1():
-    """Can only visit small caves at most once. Large caves can be visited multiple time.
-    How many distinct paths can we make?
-    Iterative function with queue of destinations."""
-    paths = paths_from_node_given_visited(node=START_CAVE, visited=set())
+    """Can only visit small caves at most once. Large caves can be visited multiple times.
+    How many distinct paths can we make?"""
+    paths = paths_from_node_given_visited(node=START_CAVE, visited=defaultdict(int), is_part1=True)
     return len(paths)
 
 
-def paths_from_node_given_visited(node: str, visited: Set[str]) -> List[List[str]]:
+def part2():
+    """Can visit small caves at most twice. Large caves can be visited multiple times.
+    How many distinct paths can we make?"""
+    paths = paths_from_node_given_visited(node=START_CAVE, visited=defaultdict(int), is_part1=False)
+    return len(paths)
+
+
+def paths_from_node_given_visited(node: str, visited: Dict[str, int], is_part1: bool) -> List[List[str]]:
     """How can I move further from the current path?
     The last item is the last node visited."""
     visited_copy = visited.copy()
-    # print(node, visited)
     if node == END_CAVE:
         # Do not move further
         return [[node]]
 
-    visited_copy.add(node)
-
+    visited_copy[node] += 1
 
     possible_destinations = set()
     for destination in grid[node]:
-        if destination in small_caves and destination in visited_copy:
+        if destination == START_CAVE:
+            # Start cave can never be a destination.
+            continue
+        elif is_part1 is True and destination in small_caves and visited_copy[destination] >= 1:
             # Small caves that are already visited are not added to possible destinations.
             continue
+        elif is_part1 is False and destination in small_caves and visited_copy[destination] >= 1 \
+                and max([visited_copy[k] for k in small_caves]) == 2:
+            # Only one small cave can be visited twice.
+            continue
         possible_destinations.add(destination)
-    # print(possible_destinations)
 
     all_future_paths = list()
     while possible_destinations:
-
         # Add destination to current path, and move further from there.
         destination = possible_destinations.pop()
-        future_paths_from_node = paths_from_node_given_visited(destination, visited_copy)
+        future_paths_from_node = paths_from_node_given_visited(destination, visited_copy, is_part1)
         all_future_paths.extend([[node] + path for path in future_paths_from_node])
     return all_future_paths
 
 
 if __name__ == '__main__':
     # Sample data
-    RAW = """start-A
+    RAW1 = """start-A
 start-b
 A-c
 A-b
@@ -87,7 +97,7 @@ b-d
 A-end
 b-end"""
 
-    RAW = """dc-end
+    RAW2 = """dc-end
 HN-start
 start-kj
 dc-start
@@ -98,7 +108,7 @@ kj-sa
 kj-HN
 kj-dc"""
 
-    RAW = """fs-end
+    RAW3 = """fs-end
 he-DX
 fs-he
 start-DX
@@ -116,16 +126,19 @@ he-WI
 zg-he
 pj-fs
 start-RW"""
-    data = parse_data(RAW)
 
-    grid = make_grid()
-    all_caves, small_caves, large_caves = make_cave_types(grid)
+    for raw, ans1, ans2 in zip((RAW1, RAW2, RAW3), (10, 19, 226), (36, 103, 3509)):
+        data = parse_data(raw)
+        grid = make_grid()
+        all_caves, small_caves, large_caves = make_cave_types(grid)
 
-    # Assert solution is correct
-    assert part1() == 226
+        # Assert solution is correct
+        assert part1() == ans1, 'Test 1 fails'
+        assert part2() == ans2, 'Test 2 fails'
+    print('All tests pass')
 
     # Actual data
-    RAW = load_data('input.txt')
+    RAW = load_data('day12.txt')
     data = parse_data(RAW)
 
     grid = make_grid()
@@ -135,3 +148,4 @@ start-RW"""
     print(f'Part 1: {part1()}')
 
     # Part 2
+    print(f'Part 2: {part2()}')
