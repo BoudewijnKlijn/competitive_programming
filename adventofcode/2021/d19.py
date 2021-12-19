4d247d3123cb71985397e0a5e4551bb888ff4a2f
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-19 13:34:20 +0100

    orientations correct

--- adventofcode/2021/d19.py
+++ adventofcode/2021/d19.py
@@ -27,57 +27,45 @@ def parse_data(raw_data: str) -> List[List[Union[Tuple[int, ]]]]:
     return all_coordinates
 
 
-def change_orientation(coordinates: List[Tuple[int, ]],):
-    coordinates = deepcopy(coordinates)
-    n_dimensions = len(coordinates[0])
-    list(permutations(range(3), 3))
-
-    # x could be either x, y, z and each of those could be positive or negative, which gives 3 * 2 options
-    # y could then be one of 2 left-over dimensions and again positive or negative, which gives another 2 * 2 options
-    # z is the remaining dimension in positive or negative, which gives 1 * 2 options
-    # In total that gives: 3 * 2 * 2 * 2 * 1 * 2 = 48 orientations, which is not 24 as described in problem statement
-
-    pass
-
-
-def rotate_around_one_axis(orientation, angle):
-    if angle == 0:
-        return orientation[0], orientation[1]
-    elif angle == 90:
-        return -1 * orientation[1], orientation[0]
-    elif angle == 180:
-        return -1 * orientation[0], -1 * orientation[1]
-    elif angle == 270:
-        return -1 * orientation[1], -1 * orientation[0]
-
-
-def create_orientations(orientations):
-    new_orientations = set()
-    for orientation in orientations:
-        for dimension in range(3):
-            for angle in [0, 90, 180, 270]:
-                # Keep one dimension fixed and rotate the others around the fixed one.
-                if dimension == 0:
-                    rotated_dimensions = rotate_around_one_axis((orientation[1], orientation[2]), angle)
-                    new_orientation = orientation[dimension], rotated_dimensions[0], rotated_dimensions[1]
-                elif dimension == 1:
-                    rotated_dimensions = rotate_around_one_axis((orientation[0], orientation[2]), angle)
-                    new_orientation = rotated_dimensions[0], orientation[dimension], rotated_dimensions[1]
-                else:
-                    rotated_dimensions = rotate_around_one_axis((orientation[0], orientation[1]), angle)
-                    new_orientation = rotated_dimensions[0], rotated_dimensions[1], orientation[dimension]
-                new_orientations.add(new_orientation)
-    return new_orientations
+ORIENTATIONS = {
+    (1, 2, 3),  # 1 FACING RIGHT (TO X)
+    (1, -3, 2),
+    (1, -2, -3),
+    (1, 3, -2),
+    (-3, 2, 1),  # 1 FACING BACK (TO Z)
+    (-2, -3, 1),
+    (3, -2, 1),
+    (2, 3, 1),
+    (-1, 2, -3),  # 1 FACING LEFT (TO -X)
+    (-1, -3, -2),
+    (-1, -2, 3),
+    (-1, 3, 2),
+    (3, 2, -1),  # 1 FACING FORWARD (TO -Z)
+    (2, -3, -1),
+    (-3, -2, -1),
+    (-2, 3, -1),
+    (-2, 1, 3),  # 1 FACING UP (TO Y)
+    (-3, 1, -2),
+    (2, 1, -3),
+    (3, 1, 2),
+    (2, -1, 3),  # 1 FACING DOWN (TO -Y)
+    (3, -1, -2),
+    (-2, -1, -3),
+    (-3, -1, 2),
+}
 
 
-ORIENTATIONS = create_orientations({(0, 1, 2)})
-print(len(ORIENTATIONS))
-ORIENTATIONS = create_orientations(ORIENTATIONS)
-print(len(ORIENTATIONS))
-ORIENTATIONS = create_orientations(ORIENTATIONS)
-print(len(ORIENTATIONS))
-print(ORIENTATIONS)
-
+def change_orientation(coordinates: List[Tuple[int, ]], orientation: Tuple[int, int, int]):
+    """Change orientation of coordinates.
+    Orientation consists of the dimension and its sign."""
+    dimensions = tuple(map(lambda o: abs(o) - 1, orientation))
+    signs = tuple(map(lambda x: 1 if x > 0 else -1, orientation))
+    new_coordinates = []
+    for coordinate in coordinates:
+        new_coordinate = signs[0] * coordinate[dimensions[0]], signs[1] * coordinate[dimensions[1]], signs[2] * \
+                         coordinate[dimensions[2]]
+        new_coordinates.append(new_coordinate)
+    return new_coordinates
 
 
 def compare_coordinates(coordinates_a: List[Tuple[int, ]], coordinates_b: List[Tuple[int, ]]) -> bool:
@@ -86,12 +74,7 @@ def compare_coordinates(coordinates_a: List[Tuple[int, ]], coordinates_b: List[T
     We are searching for beacons that the scanners have in common. There should be 12 of them overlapping.
     Instead of using the scanner as reference, we can also use a beacon coordinate as reference. Then we assume it
     matches with one of the coordinates of the other scanner. If in some orientation 12 vectors are overlapping, we have
-    found the correct orientation and overlapping beacons."""
-    # leave orientation of coordinates of A as is
-
-    # change orientation of the coordinates of B
-
-    # assume both scanners now have the correct orientation
+    found the correct orientation and overlapping beacons.
 
     # assume one of the scanned coordinates of both A and B are the same, and use this as reference point
     # express all coordinates of A and B relative to the reference point
@@ -99,9 +82,26 @@ def compare_coordinates(coordinates_a: List[Tuple[int, ]], coordinates_b: List[T
     # if less than 11, then the reference point does not overlap, pick another reference point
     # if none of the reference points overlap, then the scanners are not in the same orientation
     # if with none of the orientations we can find a suitable reference point and 11 other overlapping vectors, the
-    # scanner scan no overlapping region.
+    # scanner scan no overlapping region."""
 
-    return True
+    for orientation in ORIENTATIONS:
+        # Leave orientation of coordinates of A as is and change orientation of the coordinates of B. Assume both
+        # scanners now have the same orientation.
+        coordinates_b = change_orientation(coordinates_b, orientation)
+
+        for reference_coordinate_a, reference_coordinate_b in product(coordinates_a, coordinates_b):
+            # Assume that the reference point is the same for both scanners.
+            # Express all coordinates of A and B relative to the reference point.
+            coordinates_a_adjusted = {tuple(map(lambda pos, ref: pos - ref, coordinates, reference_coordinate_a))
+                                      for coordinates in coordinates_a if coordinates != reference_coordinate_a}
+            coordinates_b_adjusted = {tuple(map(lambda pos, ref: pos - ref, coordinates, reference_coordinate_b))
+                                      for coordinates in coordinates_b if coordinates != reference_coordinate_b}
+            # A and B should have 11 remaining overlapping vectors, since we already assume the reference point overlaps
+            # if less than 11, then the reference point does not overlap, pick another reference point
+            if len(coordinates_a_adjusted.intersection(coordinates_b_adjusted)) >= 11:
+                print(reference_coordinate_a, reference_coordinate_b)
+                return True, orientation
+    return False, None
 
 
 if __name__ == '__main__':
@@ -146,27 +146,169 @@ if __name__ == '__main__':
 -6,-4,-5
 0,7,-8
 """
+    data = parse_data(RAW)
+    all_possible_coordinates = list()
+    for i, orientation in enumerate(ORIENTATIONS):
+        possible = change_orientation(data[0], orientation)
+        all_possible_coordinates.append(possible)
+    for i in range(len(data)):
+        assert data[i] in all_possible_coordinates
+    print('Change orientation tests pass.')
+
 
     RAW = """--- scanner 0 ---
-0,2
-4,1
-3,3
+404,-588,-901
+528,-643,409
+-838,591,734
+390,-675,-793
+-537,-823,-458
+-485,-357,347
+-345,-311,381
+-661,-816,-575
+-876,649,763
+-618,-824,-621
+553,345,-567
+474,580,667
+-447,-329,318
+-584,868,-557
+544,-627,-890
+564,392,-477
+455,729,728
+-892,524,684
+-689,845,-530
+423,-701,434
+7,-33,-71
+630,319,-379
+443,580,662
+-789,900,-551
+459,-707,401
 
 --- scanner 1 ---
--1,-1
--5,0
--2,1"""
+686,422,578
+605,423,415
+515,917,-361
+-336,658,858
+95,138,22
+-476,619,847
+-340,-569,-846
+567,-361,727
+-460,603,-452
+669,-402,600
+729,430,532
+-500,-761,534
+-322,571,750
+-466,-666,-811
+-429,-592,574
+-355,545,-477
+703,-491,-529
+-328,-685,520
+413,935,-424
+-391,539,-444
+586,-435,557
+-364,-763,-893
+807,-499,-711
+755,-354,-619
+553,889,-390
+
+--- scanner 2 ---
+649,640,665
+682,-795,504
+-784,533,-524
+-644,584,-595
+-588,-843,648
+-30,6,44
+-674,560,763
+500,723,-460
+609,671,-379
+-555,-800,653
+-675,-892,-343
+697,-426,-610
+578,704,681
+493,664,-388
+-671,-858,530
+-667,343,800
+571,-461,-707
+-138,-166,112
+-889,563,-600
+646,-828,498
+640,759,510
+-630,509,768
+-681,-892,-333
+673,-379,-804
+-742,-814,-386
+577,-820,562
+
+--- scanner 3 ---
+-589,542,597
+605,-692,669
+-500,565,-823
+-660,373,557
+-458,-679,-417
+-488,449,543
+-626,468,-788
+338,-750,-386
+528,-832,-391
+562,-778,733
+-938,-730,414
+543,643,-506
+-524,371,-870
+407,773,750
+-104,29,83
+378,-903,-323
+-778,-728,485
+426,699,580
+-438,-605,-362
+-469,-447,-387
+509,732,623
+647,635,-688
+-868,-804,481
+614,-800,639
+595,780,-596
+
+--- scanner 4 ---
+727,592,562
+-293,-554,779
+441,611,-461
+-714,465,-776
+-743,427,-804
+-660,-479,-426
+832,-632,460
+927,-485,-438
+408,393,-506
+466,436,-512
+110,16,151
+-258,-428,682
+-393,719,612
+-211,-452,876
+808,-476,-593
+-575,615,604
+-485,667,467
+-680,325,-822
+-627,-443,-432
+872,-547,-609
+833,512,582
+807,604,487
+839,-516,451
+891,-625,532
+-652,-548,-490
+30,-46,-14"""
     data = parse_data(RAW)
-    print(data)
 
+    # Manual check if correct scanners see each other.
+    for scanner_i, scanner_j in product(range(len(data)), repeat=2):
+        if scanner_i == scanner_j:
+            continue
+        see_each_other, orientation = compare_coordinates(data[scanner_i], data[scanner_j])
+        if see_each_other:
+            print(f'Scanner {scanner_i} and {scanner_j} see each other. {orientation=}')
 
-
-    # Assert solution is correct
-
-    # Actual data
-    # RAW = load_data('input.txt')
-    # data = parse_data(RAW)
-
-    # Part 1
-
-    # Part 2
+#
+#     # Assert solution is correct
+#
+#     # Actual data
+#     # RAW = load_data('input.txt')
+#     # data = parse_data(RAW)
+#
+#     # Part 1
+#
+#     # Part 2
