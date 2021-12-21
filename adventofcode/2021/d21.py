aba8d6d302e5230ba45db2a05dbd2a78067e44e8
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-21 12:03:27 +0100

    counts of when games are end or not correct because if one player reaches 21, all universes of the other need to stop as well, thereby reducing the number of games that end in n_turns higher than when the other player ended

--- adventofcode/2021/d21.py
+++ adventofcode/2021/d21.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from itertools import cycle
 from typing import Tuple
 from collections import Counter
+from copy import deepcopy
+import itertools
 
 
 def load_data(filename: str) -> str:
@@ -20,6 +22,7 @@ class Player:
     id: int
     position: int
     score: int = 0
+    n_turns: int = 0
 
     def move(self, throw):
         self.position += throw
@@ -28,22 +31,26 @@ class Player:
 
     def increase_score(self):
         self.score += self.position
+        self.n_turns += 1
 
     def __eq__(self, other):
-        return self.id == other.id and self.position == other.position and self.score == other.score
+        return self.position == other.position and self.score == other.score and self.n_turns == other.n_turns
 
     def __hash__(self):
-        return hash((self.id, self.position, self.score))
+        return hash((self.id, self.position, self.score, self.n_turns))
 
 
 class Dice:
     def __init__(self):
         self.rolls = cycle(range(1, 100 + 1))
-        self.turns = 0
+        self.n_rolls = 0
 
     def roll(self):
-        self.turns += 1
+        self.n_rolls += 1
         return next(self.rolls)
+
+
+DIRAC_SUM_THROWS = Counter(map(sum, itertools.product([1, 2, 3], repeat=3)))
 
 
 def play_game():
@@ -55,7 +62,29 @@ def play_game():
             player.move(sum_throws)
             player.increase_score()
             if player.score >= 1000:
-                return min(p.score for p in players) * dice.turns
+                return min(p.score for p in players) * dice.n_rolls
+
+
+def determine_turns_to_end_game(start):
+    turn_counter = Counter()
+    state_counts = Counter([start])
+    while True:
+        new_state = False
+        new_state_counts = Counter()
+        for player_state, count in state_counts.items():
+            if player_state.score >= 21:
+                turn_counter += Counter({player_state.n_turns: count})
+                continue
+            new_state = True
+            for sum_throws, n_possible_ways_to_throw_sum in DIRAC_SUM_THROWS.items():
+                new_state = deepcopy(player_state)
+                new_state.move(sum_throws)
+                new_state.increase_score()
+                new_state_counts += Counter({new_state: count * n_possible_ways_to_throw_sum})
+        state_counts = deepcopy(new_state_counts)
+
+        if not new_state:
+            return turn_counter
 
 
 def part2():
@@ -63,16 +92,21 @@ def part2():
     took to get to 21. If turns tie, player 1 wins. Use counter to combine game and avoid explosion of number of games.
     """
     players = [Player(i, p) for i, p in enumerate(data)]
-    player1_counts = Counter([players[0]])
-    player2_counts = Counter([players[1]])
+    turns = list()
+    for player_i, player in enumerate(players):
+        turn_counter = determine_turns_to_end_game(player)
+        turns.append(turn_counter)
+    print(turns)
 
-    for player in players:
-        # one turn creates three universes
-        p = [Player(0, data[0]) for _ in range(3)]
-        a = Counter(p)
-        print(a)
-
-
+    # Compare all turns of one player to end the game with that of the other player.
+    game_wins = Counter({'p1': 0, 'p2': 0})
+    for p1_turns, p1_count in turns[0].items():
+        for p2_turns, p2_count in turns[1].items():
+            if p1_turns <= p2_turns:
+                game_wins['p1'] += p1_count * p2_count
+            else:
+                game_wins['p2'] += p1_count * p2_count
+    print(game_wins)
 
 
 if __name__ == '__main__':
@@ -85,8 +119,10 @@ Player 2 starting position: 8"""
     assert play_game() == 739785
 
     # Actual data
-    RAW = load_data('input.txt')
-    data = parse_data(RAW)
+    # RAW = load_data('input.txt')
+    # data = parse_data(RAW)
+
+
 
     # Part 1
     print(f'Part 1: {play_game()}')
