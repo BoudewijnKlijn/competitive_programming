3e412a5769548c50a773dc720da057878d1eab87
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2021-12-23 09:16:31 +0100

    day 23 both parts by hand

---
+++ adventofcode/2021/d23.py
@@ -0,0 +1,5 @@
+"""Solved by hand.
+
+Due to the very different cost of steps (1, 10, 100, 1000) and the limited movement space (not allowed to wait directly
+outside of the room, only allowed to move once before moving back into the room), there is a limited number of solutions
+you have to consider."""
