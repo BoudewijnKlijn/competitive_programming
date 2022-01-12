a1c33414285299ddc961a6771b286249255c4c47
sebastianpdw <sebastianpanmandewit@gmail.com>
2022-01-12 20:17:19 +0100

    Adding a new scorer

--- HC_2022_Warmup/workspace_sebastian2.py
+++ HC_2022_Warmup/workspace_sebastian2.py
@@ -4,6 +4,7 @@ import time
 
 from HC_2022_Warmup.perfect_pizza_score import PerfectPizzaScore
 from HC_2022_Warmup.pizza_demands import PizzaDemands
+from HC_2022_Warmup.strategies.easy_customers import EasyCustomers
 from HC_2022_Warmup.strategies.valuable_ingredients import ValuableIngredients
 
 THIS_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -19,7 +20,7 @@ if __name__ == '__main__':
         demands = PizzaDemands(os.path.join(directory, problem_file))
 
         scorer = PerfectPizzaScore(demands)
-        strategy = ValuableIngredients(scorer, seed=27)
+        strategy = EasyCustomers(scorer, seed=27)
         start = time.perf_counter()
         solution = strategy.solve(demands)
         duration = time.perf_counter() - start
@@ -33,19 +34,3 @@ if __name__ == '__main__':
     print("----------------------")
     print()
 
-    """
-    for problem_file in files:
-        print(f"Trying to solve file (BACKWARD): {problem_file}")
-        demands = PizzaDemands(os.path.join(directory, problem_file))
-
-        scorer = PerfectPizzaScore(demands)
-        strategy = ValuableIngredients(scorer, forward_selection=False, seed=27)
-        start = time.perf_counter()
-        solution = strategy.solve(demands)
-        duration = time.perf_counter() - start
-
-        score = scorer.calculate(solution)
-
-        print(f'{problem_file} Score: {score} ({duration:0.0f}s)')
-        print("----------------------")
-    """
