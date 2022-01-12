21c1717186d1543ea7672f37094a5294285d00ea
Nozziel <nozziel.u.shadow@gmail.com>
2022-01-12 20:31:46 +0100

    saving stuff

--- valcon/strategies/strategy.py
+++ valcon/strategies/strategy.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
 from random import Random
 
 from valcon import OutputData, InputData
@@ -7,7 +7,12 @@ from valcon import OutputData, InputData
 class Strategy(ABC):
     """ don't forget to call super().__init__()"""
 
+    @property
+    def name(self):
+        return f'{type(self).__name__}({self.seed})'
+
     def __init__(self, seed=27):
+        self.seed = seed
         self.random = Random()
         self.random.seed(seed)
 
--- valcon/utils.py
+++ valcon/utils.py
@@ -1,2 +1,21 @@
+import glob
+import os
+
+
 def flatten(t):
     return [item for sublist in t for item in sublist]
+
+
+def best_score(directory: str):
+    problems = ['a', 'b', 'c', 'd', 'e']
+
+    solutions = glob.glob(os.path.join(directory, '*.txt'))
+    file_names = [os.path.basename(f) for f in solutions]
+    split = [f.split('-') for f in file_names]
+
+    best_scores = dict()
+
+    for problem in problems:
+        scores = [int(s[1]) for s in split if s[0] == problem in s]
+        best_scores[problem] = max(scores) if scores else 0
+    return best_scores
