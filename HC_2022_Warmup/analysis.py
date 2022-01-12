2053b2418d06fe185d9f58572cfae3c2ce58ae9b
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2022-01-12 19:47:01 +0100

    more stats

--- HC_2022_Warmup/analysis.py
+++ HC_2022_Warmup/analysis.py
@@ -1,5 +1,6 @@
+import json
 import os
-
+from collections import Counter
 from HC_2022_Warmup.pizza_demands import PizzaDemands
 
 THIS_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -13,12 +14,18 @@ if __name__ == '__main__':
         'd_difficult.in.txt',
         'e_elaborate.in.txt',
     ]
+    n_most_common = 20
+
     for problem_file in files:
         demands = PizzaDemands(os.path.join(directory, problem_file))
-        unique_likes = set()
-        unique_dislikes = set()
+        unique_likes = Counter()
+        unique_dislikes = Counter()
         for customer in demands.customers:
-            unique_likes.update(customer.likes)
-            unique_dislikes.update(customer.dislikes)
+            unique_likes.update(Counter(customer.likes))
+            count_likes = Counter(unique_likes.values())
+            unique_dislikes.update(Counter(customer.dislikes))
 
-        print(f'{problem_file:<20}: {len(unique_likes):8d} likes, {len(unique_dislikes):8d} dislikes')
+        print(f'\n{problem_file}\n{len(unique_likes.keys()):8d} likes, {len(unique_dislikes.keys()):8d} dislikes')
+        print(f'{n_most_common} most common LIKES: {unique_likes.most_common(n_most_common)}')
+        print(f'{n_most_common} most common DISLIKES: {unique_dislikes.most_common(n_most_common)}')
+        print(f'Occurrences of occurrences {json.dumps(dict(sorted(count_likes.items())), indent=4)}')
