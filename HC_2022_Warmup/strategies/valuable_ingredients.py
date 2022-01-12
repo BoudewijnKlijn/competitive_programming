a6e7558a600839d2c8d83dafd4c7d24301d74470
sebastianpdw <sebastianpanmandewit@gmail.com>
2022-01-12 19:44:14 +0100

    Added backward selection

--- HC_2022_Warmup/strategies/valuable_ingredients.py
+++ HC_2022_Warmup/strategies/valuable_ingredients.py
@@ -5,12 +5,12 @@ from valcon.utils import flatten
 
 
 class ValuableIngredients(Strategy):
-    def __init__(self, scorer, seed=None):
+    def __init__(self, scorer, seed=None, forward_selection=True):
         super().__init__(seed)
         self.scorer = scorer
+        self.forward_selection = forward_selection
 
-    @staticmethod
-    def _get_valuable_ingredients(customers: [Customer]):
+    def _get_valuable_ingredients(self, customers: [Customer]):
         ingredients = set(flatten(customer.likes for customer in customers))
         ingredient_counts = {}
         for ingredient in ingredients:
@@ -18,11 +18,20 @@ class ValuableIngredients(Strategy):
             nr_dislikes = sum([1 for customer in customers if ingredient in customer.dislikes])
             ingredient_counts[ingredient] = nr_likes - nr_dislikes
 
-        return dict(reversed(sorted(ingredient_counts.items(), key=lambda item: item[1])))
+        if self.forward_selection:
+            return dict(reversed(sorted(ingredient_counts.items(), key=lambda item: item[1])))
+        else:
+            return dict(reversed(sorted(ingredient_counts.items(), key=lambda item: item[1])))
 
     def solve(self, input_data: PizzaDemands) -> PerfectPizza:
+        if self.forward_selection:
+            return self.solve_forward(input_data)
+        else:
+            return self.solve_backward(input_data)
+
+    def solve_forward(self, input_data: PizzaDemands) -> PerfectPizza:
         valuable_ingredients = self._get_valuable_ingredients(input_data.customers)
-        #print(f"Ingredient values: {valuable_ingredients}")
+        # print(f"Ingredient values: {valuable_ingredients}")
         highest_score = 0
         current_ingredients = []
         for ingredient, value in valuable_ingredients.items():
@@ -33,6 +42,24 @@ class ValuableIngredients(Strategy):
 
             if score < highest_score:
                 break
-        #print(f"Final ingredients: {current_ingredients}")
+        # print(f"Final ingredients: {current_ingredients}")
+        # chosen = self.random.sample(ingredients, self.nr_ingredients)
+        return PerfectPizza(current_ingredients)
+
+    def solve_backward(self, input_data: PizzaDemands) -> PerfectPizza:
+        valuable_ingredients = self._get_valuable_ingredients(input_data.customers)
+        # print(f"Ingredient values: {valuable_ingredients}")
+
+        current_ingredients = list(valuable_ingredients.keys())
+        highest_score = self.scorer.calculate(PerfectPizza(current_ingredients))
+        for ingredient, value in valuable_ingredients.items():
+            # print(f"Adding the following ingredient: {ingredient} with value: {value}")
+            current_ingredients.remove(ingredient)
+            score = self.scorer.calculate(PerfectPizza(current_ingredients))
+            # print(f"Current score: {score}")
+
+            if score < highest_score:
+                break
+        # print(f"Final ingredients: {current_ingredients}")
         # chosen = self.random.sample(ingredients, self.nr_ingredients)
         return PerfectPizza(current_ingredients)
