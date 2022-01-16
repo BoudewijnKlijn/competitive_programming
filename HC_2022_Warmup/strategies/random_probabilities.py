11b0fd36451669a8f40ee72717b4134399abbbf7
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2022-01-16 11:27:07 +0100

    use numpy

--- HC_2022_Warmup/strategies/random_probabilities.py
+++ HC_2022_Warmup/strategies/random_probabilities.py
@@ -1,7 +1,6 @@
 import os
 
-from scipy.special import softmax
-from numpy.linalg import norm
+import numpy as np
 
 from HC_2022_Warmup.perfect_pizza import PerfectPizza
 from HC_2022_Warmup.pizza_demands import PizzaDemands
@@ -15,12 +14,11 @@ class RandomClientProbability(Strategy):
     def __init__(self, seed, n_clients, customer_probabilities):
         super().__init__(seed)
         self.n_clients = n_clients
-        self.customer_probabilities = customer_probabilities
-        if sum(self.customer_probabilities) != 1:
-            # Apply softmax to probabilities so that they sum to 1.
-            # self.customer_probabilities = softmax(self.customer_probabilities)
-            sum_ = sum(self.customer_probabilities)
-            self.customer_probabilities = list(map(lambda x: x / sum_, self.customer_probabilities))
+        self.customer_probabilities = np.array(customer_probabilities)
+        if self.customer_probabilities.min() < 0:
+            raise ValueError("Probabilities must be non-negative")
+        if self.customer_probabilities.sum() != 1:
+            self.customer_probabilities /= self.customer_probabilities.sum()
 
     def solve(self, input_data: PizzaDemands) -> PerfectPizza:
         random_customer_ids = self.rng.choice(
