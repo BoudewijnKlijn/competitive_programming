44f370fea9db71bbcd5bda4745b9c360ed97d5f1
Marco van der Linden <marco.vanderlinden@viqtordavis.com>
2022-02-24 18:45:48 +0100

    inputs

--- HC_2022_Qualification/problem_data.py
+++ HC_2022_Qualification/problem_data.py
@@ -5,3 +5,5 @@ class ProblemData(InputData):
     def __init__(self, file_name: str):
         with open(file_name, 'r') as file:
             meta_data, *raw_data = file.readlines()
+
+        self.
