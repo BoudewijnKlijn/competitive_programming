47521e5773e422525fe5faf330dc4c713ce0f4bd
sebastianpdw <sebastianpanmandewit@gmail.com>
2022-02-24 18:25:20 +0100

    Adding baseline strategy

---
+++ HC_2022_Qualification/strategies/baseline_strategy.py
@@ -0,0 +1,18 @@
+import random
+
+from .base_strategy import BaseStrategy
+from ..problem_data import ProblemData
+from ..solution import Solution
+
+
+class BaselineStrategy(BaseStrategy):
+
+    def __init__(self, seed: int = None):
+        if seed:
+            self.seed = seed
+        else:
+            self.seed = random.randint(0, 999_999_999)
+        super().__init__(seed)
+
+    def solve(self, input_data: ProblemData) -> Solution:
+        pass
