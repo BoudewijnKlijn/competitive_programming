39aef27306a205e0625af3afad1f6cb907feb593
Boudewijn Klijn <boudewijn.klijn@valcon.com>
2022-02-24 21:50:13 +0100

    runner fixed with baseline strat v2

---
+++ HC_2022_Qualification/strategies/random_strategy_v2.py
@@ -0,0 +1,31 @@
+import random
+
+from .base_strategy import BaseStrategy
+from .baseline_strategy_v2 import BaselineStrategy
+from ..problem_data import ProblemData
+from ..solution import Solution
+
+
+class RandomStrategy(BaseStrategy):
+
+    def __init__(self, seed: int = None):
+        """
+        Initializes a RandomStrategy which assign random contributors to a project
+
+        Args:
+            seed (int): Seed for random generator
+        """
+        if seed:
+            self.seed = seed
+        else:
+            self.seed = random.randint(0, 999_999_999)
+        random.seed = self.seed
+        super().__init__(seed)
+
+    def solve(self, input_data: ProblemData) -> Solution:
+
+        random.shuffle(input_data.contributors)
+
+        baseline_strategy = BaselineStrategy()
+
+        return baseline_strategy.solve(input_data)
