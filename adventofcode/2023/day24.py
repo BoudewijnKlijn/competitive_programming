cfff1c606e929e5790ea08d858f5e4c04462ed1d
unknown <boudewijn_klijn@hotmail.com>
2024-06-10 14:32:28 +0200

    2nd approach doesn't work either

--- adventofcode/2023/day24.py
+++ adventofcode/2023/day24.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sympy import factorint
+from sklearn.linear_model import LinearRegression
 
 pd.set_option("display.max_columns", None)
 
@@ -50,204 +51,59 @@ def part1(content, bounds=(7, 27)):
 
 
 def part2(content):
+    """We are looking for a straight line that passes all other 3D lines.
+    To start, I ignore the time, I just try to find a line that goes through all other lines.
+    Once I found that line I will derive the starting point and velocities, such that the line perfectly collides.
+    To find the line that intersects all others:
+    - Some point ABC intersects with a hail line if the following holds:
+        A/vx - x0/vx = B/vy - y0/vy = C/vz - z0/vz
+    - Then I just set A to some value and solve for B and C.
+    - If not all B and C points are on a line, I have to adjust the A value.
+
+    Loss doesn't become zero, so approach doesn't work.
+    """
     hailstones = list()
     for line in content.split("\n"):
         data = list(map(int, re.findall(r"-?\d+", line)))
         hailstones.append(data)
 
-    use_scaler = False
 
-    df = pd.DataFrame(hailstones)
-    scaler = StandardScaler()
-    df_transformed = pd.DataFrame(scaler.fit_transform(df))
 
-    # rock starting values
-    xr0, yr0, zr0, dxrdt, dyrdt, dzrdt = 0, 0, 0, 0, 0, 0
-    xr0, yr0, zr0, dxrdt, dyrdt, dzrdt = 24, 13, 10, -3, 1, 2
-    # xr0, yr0, zr0, dxrdt, dyrdt, dzrdt = 23, 13, 10, -3, 1, 2
+    model = LinearRegression()
 
-    x10, y10, z10, dx1dt, dy1dt, dz1dt = df.iloc[0].values
-
-    txs = list()
-    tys = list()
+    # find the line that intersects all other lines
     losses = list()
-    dxrdts = list()
-    dyrdts = list()
-    df0 = pd.DataFrame(columns=["xr0", "yr0", "zr0", "dxrdt", "dyrdt", "dzrdt", "loss"])
+    # as_ = list(range(10, 20))
+    bs = np.linspace(18, 20, 100)
+    for b in bs:
 
-    for dxrdt, dyrdt in product(range(-3, 4), repeat=2):
-        dxrdts.append(dxrdt)
-        dyrdts.append(dyrdt)
-        tx = (x10 - xr0) / (dxrdt - dx1dt) if (dxrdt - dx1dt) != 0 else np.nan
-        ty = (y10 - yr0) / (dyrdt - dy1dt) if (dyrdt - dy1dt) != 0 else np.nan
+        as_ = list()
+        # bs = list()
+        cs = list()
+        for i, hailstone in enumerate(hailstones):
+            x0, y0, z0, vx, vy, vz = hailstone
 
-        df0 = df0._append(
-            {
-                "xr0": xr0,
-                "yr0": yr0,
-                "zr0": zr0,
-                "dxrdt": dxrdt,
-                "dyrdt": dyrdt,
-                "dzrdt": dzrdt,
-                "loss": (tx - ty) ** 2,
-            },
-            ignore_index=True,
-        )
+            # b = y0 + vy * (a - x0) / vx
+            # c = z0 + vz * (a - x0) / vx
+            a = x0 + vx * (b - y0) / vy
+            c = z0 + vz * (b - y0) / vy
 
-        loss = (tx - ty) ** 2
-        print(dxrdt, dyrdt, tx, ty, loss)
-        txs.append(tx)
-        tys.append(ty)
+            as_.append(a)
+            # bs.append(b)
+            cs.append(c)
+
+            # print(a, b, c)
+
+        # break
+        model.fit(np.array(as_).reshape(-1, 1), np.array(cs).reshape(-1, 1))
+        pred = model.predict(np.array(as_).reshape(-1, 1))
+        loss = np.mean((np.array(cs) - pred) ** 2)
         losses.append(loss)
-
-    # plt.plot(dxrdts, losses, label="tx")
-    # plt.show()
-    # plt.plot(dyrdts, losses, label="tx")
-    # plt.show()
-
-    sns.lineplot(x="dxrdt", y="loss", data=df0)
-    plt.show()
-    sns.lineplot(x="dyrdt", y="loss", data=df0)
+        print(b, loss)
+    plt.plot(bs, losses)
     plt.show()
 
-    exit()
 
-    if use_scaler:
-        df_rock = pd.DataFrame(np.array([[xr0, yr0, zr0, dxrdt, dyrdt, dzrdt]]))
-        df_rock_transformed = pd.DataFrame(scaler.transform(df_rock))
-        xr0, yr0, zr0, dxrdt, dyrdt, dzrdt = df_rock_transformed.iloc[0].values
-
-    learning_rate = 1e-2
-    n = len(hailstones)
-    epochs = 2
-    total_losses = list()
-
-    for _ in range(epochs):
-        # print(xr0, yr0, zr0, dxrdt, dyrdt, dzrdt)
-        loss = list()
-
-        dloss_dxr0 = 0
-        dloss_dyr0 = 0
-        dloss_dzr0 = 0
-        dloss_ddxrdt = 0
-        dloss_ddyrdt = 0
-        dloss_ddzrdt = 0
-
-        for i in range(n):
-            print("hailstone", i)
-            x10, y10, z10, dx1dt, dy1dt, dz1dt = df.iloc[i].values
-            if use_scaler:
-                x10, y10, z10, dx1dt, dy1dt, dz1dt = df_transformed.iloc[i].values
-
-            tx = (x10 - xr0) / (dxrdt - dx1dt) if (dxrdt - dx1dt) != 0 else np.nan
-            ty = (y10 - yr0) / (dyrdt - dy1dt) if (dyrdt - dy1dt) != 0 else np.nan
-            # tz = (z10 - zr0) / (dzrdt - dz1dt) if (dzrdt - dz1dt) != 0 else np.nan
-
-            if np.isnan(tx):
-                # tx = np.mean([ty, tz])
-                tx = ty
-            if np.isnan(ty):
-                # ty = np.mean([tx, tz])
-                ty = tx
-            # if np.isnan(tz):
-            #     tz = np.mean([tx, ty])
-
-            loss.append((tx - ty) ** 2)  # + (ty - tz) ** 2 + (tz - tx) ** 2)
-            print(
-                i,
-                tx,
-                ty,
-            )  # tz)
-            # print(tx, ty, tz, loss)
-
-            # determine partial derivatives of loss with respect to xr0, yr0, zr0, dxrdt, dyrdt, dzrdt
-            # dloss_dxr0 += - 1 / (dxrdt - dx1dt) * (2 * tx - ty - tz) if (dxrdt - dx1dt) != 0 else 0
-            # dloss_dyr0 += - 1 / (dyrdt - dy1dt) * (2 * ty - tx - tz) if (dyrdt - dy1dt) != 0 else 0
-            # dloss_dzr0 += - 1 / (dzrdt - dz1dt) * (2 * tz - tx - ty) if (dzrdt - dz1dt) != 0 else 0
-            # dloss_ddxrdt += -1 / (dxrdt - dx1dt) * tx * (2 * tx - ty - tz) if (dxrdt - dx1dt) != 0 else 0
-            # dloss_ddyrdt += -1 / (dyrdt - dy1dt) * ty * (2 * ty - tx - tz) if (dyrdt - dy1dt) != 0 else 0
-            # dloss_ddzrdt += -1 / (dzrdt - dz1dt) * tz * (2 * tz - tx - ty) if (dzrdt - dz1dt) != 0 else 0
-            dloss_dxr0 += (
-                -1 / (dxrdt - dx1dt) * (2 * tx - ty) if (dxrdt - dx1dt) != 0 else 0
-            )
-            dloss_dyr0 += (
-                -1 / (dyrdt - dy1dt) * (2 * ty - tx) if (dyrdt - dy1dt) != 0 else 0
-            )
-            # dloss_dzr0 += - 1 / (dzrdt - dz1dt) * (2 * tz - tx - ty) if (dzrdt - dz1dt) != 0 else 0
-            dloss_ddxrdt += (
-                -1 / (dxrdt - dx1dt) * tx * (2 * tx - ty) if (dxrdt - dx1dt) != 0 else 0
-            )
-            dloss_ddyrdt += (
-                -1 / (dyrdt - dy1dt) * ty * (2 * ty - tx) if (dyrdt - dy1dt) != 0 else 0
-            )
-            # dloss_ddzrdt += -1 / (dzrdt - dz1dt) * tz * (2 * tz - tx - ty) if (dzrdt - dz1dt) != 0 else 0
-
-            print(
-                "partial derivatives",
-                dloss_dxr0,
-                dloss_dyr0,
-                dloss_dzr0,
-                dloss_ddxrdt,
-                dloss_ddyrdt,
-                dloss_ddzrdt,
-            )
-            print("loss", loss)
-
-        total_losses.append(np.mean(loss))
-
-        # update rock starting values
-        xr0 -= learning_rate * dloss_dxr0 / n
-        yr0 -= learning_rate * dloss_dyr0 / n
-        # zr0 -= learning_rate * dloss_dzr0 / n
-        dxrdt -= learning_rate * dloss_ddxrdt / n
-        dyrdt -= learning_rate * dloss_ddyrdt / n
-        # dzrdt -= learning_rate * dloss_ddzrdt / n
-
-    df_tmp = pd.DataFrame(np.array([[xr0, yr0, zr0, dxrdt, dyrdt, dzrdt]]))
-    if use_scaler:
-        df_tmp = pd.DataFrame(scaler.transform(df_tmp))
-
-    return df_tmp.iloc[0].values
-
-
-def analysis(content):
-    hailstones = list()
-    for line in content.split("\n"):
-        data = list(map(int, re.findall(r"-?\d+", line)))
-        hailstones.append(data)
-
-    rock = [24, 13, 10, -3, 1, 2]
-
-    # # get prime factors of all x10, y10, z10
-    # for hailstone in hailstones:
-    #     x10, y10, z10, *_ = hailstone
-    #     divx = factorint(x10, multiple=True)
-    #     divy = factorint(y10)
-    #     divz = factorint(z10)
-    #     print(x10, divx)
-    #     print(y10, divy)
-    #     print(z10, divz)
-    #     print()
-
-    # # plot dim1 vs dim2
-    # times = np.arange(9)
-    # fig, ax = plt.subplots(3, 3, sharex=True, sharey=True)
-    # for t in times:
-    #     for i, (_, dim1_0, dim2_0,  _, dim1_d, dim2_d) in enumerate([rock] + hailstones):
-    #         if i == 0:
-    #             marker = "o"
-    #         else:
-    #             marker = "x"
-    #         dim1 = dim1_0 + dim1_d * times
-    #         dim2 = dim2_0 + dim2_d * times
-    #         ax[t // 3, t % 3].plot(dim1, dim2, label=f"{i}")
-    #         ax[t // 3, t % 3].scatter([dim1_0 + dim1_d * t], [dim2_0 + dim2_d * t], marker=marker)
-    # plt.legend()
-    # plt.show()
-
-    # statistics
-    df = pd.DataFrame(hailstones)
-    print(df.describe())
 
 
 if __name__ == "__main__":
@@ -264,12 +120,11 @@ if __name__ == "__main__":
 
     BOUNDS = (200000000000000, 400000000000000)
 
-    print(part1(CONTENT, bounds=BOUNDS))
+    # print(part1(CONTENT, bounds=BOUNDS))
 
     # assert part2(SAMPLE) == 47
 
-    # print(part2(SAMPLE))
+    print(part2(SAMPLE))
 
     # analysis(SAMPLE)
-    analysis(CONTENT)
     # print(part2(CONTENT))
