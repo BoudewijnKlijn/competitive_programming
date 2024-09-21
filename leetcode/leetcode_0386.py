2d17bbc0f9c90f007c12e1741e539e567d959da4
unknown <boudewijn_klijn@hotmail.com>
2024-09-21 13:14:40 +0200

    shorter code

--- leetcode/leetcode_0386.py
+++ leetcode/leetcode_0386.py
@@ -3,4 +3,4 @@ from typing import List
 
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        return list(map(int, sorted(map(str, range(1, n + 1)))))
+        return sorted(range(1, n + 1), key=str)
