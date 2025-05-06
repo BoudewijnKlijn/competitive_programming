771bd753db8db020d1b5e60515815f5945bd1db0
unknown <boudewijn_klijn@hotmail.com>
2025-05-06 11:10:30 +0200

    1920

---
+++ leetcode/leetcode_1920.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = list()
+        for num in nums:
+            ans.append(nums[num])
+        return ans
