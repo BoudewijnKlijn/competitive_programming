7c2409bbd5c1b209a7486bb2035d858494842295
unknown <boudewijn_klijn@hotmail.com>
2025-08-22 17:03:13 +0200

    leetcode 2139

---
+++ leetcode/leetcode_2139.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        """Its always best to increment first, since both yield the same, but doubling is limited.
+        Its always best to double later, rather than sooner if we hit the double limit, because
+            doubling later gains more.
+        """
+        moves = 0
+        while target > 1:
+            div, mod = divmod(target, 2)
+            if maxDoubles > 0 and not mod:
+                target = div
+                maxDoubles -= 1
+                moves += 1
+            elif not maxDoubles:
+                target = 1
+                moves += target - 1
+            else:
+                target -= 1
+                moves += 1
+        return moves
+
+
+if __name__ == "__main__":
+    from timing import timing
+
+    timing(
+        solution=Solution(),
+        funcs=["minMoves"],
+        data_file="leetcode_2139_data.txt",
+        exclude_data_lines=None,
+        check_result=True,
+    )
