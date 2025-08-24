d5a1f888911088722cf4c1d001bf7aabb35a6016
unknown <boudewijn_klijn@hotmail.com>
2025-08-24 21:27:26 +0200

    leetcode 0013

---
+++ leetcode/leetcode_0013.py
@@ -0,0 +1,31 @@
+from typing import List
+
+roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        prev = s[0]
+        ans = roman_dict[prev]
+        for char in s[1:]:
+            if prev == "I" and char in "VX":
+                ans -= 2
+            elif prev == "X" and char in "LC":
+                ans -= 20
+            elif prev == "C" and char in "DM":
+                ans -= 200
+            prev = char
+            ans += roman_dict[char]
+        return ans
+
+
+if __name__ == "__main__":
+    from timing import timing
+
+    timing(
+        solution=Solution(),
+        funcs=["romanToInt"],
+        data_file="leetcode_0013_data.txt",
+        exclude_data_lines=None,
+        check_result=True,
+    )
