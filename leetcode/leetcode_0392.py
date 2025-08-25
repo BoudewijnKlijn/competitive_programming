4475ffb1a13354e7c52351b403971c440cd329f3
unknown <boudewijn_klijn@hotmail.com>
2025-08-25 15:48:33 +0200

    leetcode 0392

---
+++ leetcode/leetcode_0392.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+        found = 0
+        n = len(s)
+        for char in t:
+            if s[found] == char:
+                found += 1
+                if found == n:
+                    return True
+        return False
