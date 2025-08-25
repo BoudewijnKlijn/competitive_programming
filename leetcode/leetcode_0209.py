0ba8980ca1ef787ae714f913c503cf4f35759663
unknown <boudewijn_klijn@hotmail.com>
2025-08-25 17:00:38 +0200

    leetcode 0209

---
+++ leetcode/leetcode_0209.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        length = 0
+        total = 0
+        best = None
+        for num in nums:
+            total += num
+            length += 1
+            while total >= target:
+                if best is None or length < best:
+                    best = length
+                total -= nums[left]
+                left += 1
+                length -= 1
+
+        return best if best else 0
+
+
+if __name__ == "__main__":
+    import os
+
+    from timing import timing
+
+    PROBLEM = "0209"
+    data_file = os.path.join(os.path.dirname(__file__), f"leetcode_{PROBLEM}_data.txt")
+
+    # # generate testcases
+    # import sys
+
+    # sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+    # from generic.helper import InputInteger, InputList, generate_testcases
+
+    # arr1 = InputList(n_min_max=(2, 100_000), val_min_max=(0, 10_000))
+    # int1 = InputInteger(val_min_max=(0, 10_000))
+    # vars = generate_testcases(structure=(arr1,), n=1, data_file=data_file, solver=None)
+
+    timing(
+        solution=Solution(),
+        funcs=["minSubArrayLen"],
+        data_file=data_file,
+        exclude_data_lines=None,
+        check_result=True,
+    )
