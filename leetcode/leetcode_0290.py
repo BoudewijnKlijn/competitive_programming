a946ae763a13f07adf746447e11af9a688632cc4
unknown <boudewijn_klijn@hotmail.com>
2025-08-27 21:28:54 +0200

    leetcode 0290 0242

---
+++ leetcode/leetcode_0290.py
@@ -0,0 +1,51 @@
+from collections import Counter
+
+
+class Solution:
+    def wordPattern(self, s: str, t: str) -> bool:
+        """Almost identical to 205."""
+        t = t.split()  # only difference compared with 205
+        if len(s) != len(t):
+            return False
+
+        mapping = dict()
+        for ss, tt in zip(s, t):
+            if tt in mapping:
+                # must map to the character it mapped before
+                if mapping[tt] != ss:
+                    return False
+            else:
+                # store mapping for unseen character
+                mapping[tt] = ss
+
+        # only one on one mapping, not multiple to one
+        if any(int(v) > 1 for v in Counter(mapping.values()).values()):
+            return False
+        return True
+
+
+if __name__ == "__main__":
+    import os
+
+    from timing import timing
+
+    PROBLEM = "0290"
+    data_file = os.path.join(os.path.dirname(__file__), f"leetcode_{PROBLEM}_data.txt")
+
+    # # generate testcases
+    # import sys
+
+    # sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+    # from generic.helper import InputInteger, InputList, generate_testcases
+
+    # arr1 = InputList(n_min_max=(2, 100_000), val_min_max=(0, 10_000))
+    # int1 = InputInteger(val_min_max=(0, 10_000))
+    # vars = generate_testcases(structure=(arr1,), n=1, data_file=data_file, solver=None)
+
+    timing(
+        solution=Solution(),
+        funcs=["wordPattern"],
+        data_file=data_file,
+        exclude_data_lines=None,
+        check_result=True,
+    )
