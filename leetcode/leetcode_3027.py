c82bd2129aec288ea552513692f691497b8f0b48
unknown <boudewijn_klijn@hotmail.com>
2025-09-03 13:25:19 +0200

    leetcode 3027

---
+++ leetcode/leetcode_3027.py
@@ -0,0 +1,55 @@
+from itertools import product
+from typing import List
+
+
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        return self.sort_first(points)
+
+    def sort_first(self, points):
+        """Same solution as 3025.
+        Sorting first, prevents checks on x and only try C between A and B."""
+        points.sort(key=lambda x: (x[0], -x[1]))
+        ans = 0
+        for i, (_, yA) in enumerate(points):
+            for j, (_, yB) in enumerate(points[i + 1 :], start=i + 1):
+                if yA < yB:
+                    # A must be upper left of B
+                    continue
+
+                # smaller than A (i) or larger than B (j) are definitely not inside rectangle
+                for _, yC in points[i + 1 : j]:
+                    if yB <= yC <= yA:
+                        # C must not be contained in rectangle of A and B
+                        break
+                else:
+                    ans += 1
+
+        return ans
+
+
+if __name__ == "__main__":
+    import os
+
+    from timing import timing
+
+    PROBLEM = "3027"
+    data_file = os.path.join(os.path.dirname(__file__), f"leetcode_{PROBLEM}_data.txt")
+
+    # # generate testcases
+    # import sys
+
+    # sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+    # from generic.helper import InputInteger, InputList, generate_testcases
+
+    # arr1 = InputList(n_min_max=(2, 100_000), val_min_max=(0, 10_000))
+    # int1 = InputInteger(val_min_max=(0, 10_000))
+    # vars = generate_testcases(structure=(arr1,), n=1, data_file=data_file, solver=None)
+
+    timing(
+        solution=Solution(),
+        funcs=["sort_first"],
+        data_file=data_file,
+        exclude_data_lines=None,
+        check_result=True,
+    )
