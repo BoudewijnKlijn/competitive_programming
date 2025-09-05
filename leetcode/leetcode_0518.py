1280d18c95eb7b50f1993c61b89c4df33c112067
unknown <boudewijn_klijn@hotmail.com>
2025-09-05 17:36:51 +0200

    leetcode 0518

---
+++ leetcode/leetcode_0518.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        coins.sort()
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+        return dp[amount]
+
+
+if __name__ == "__main__":
+    import os
+
+    from timing import timing
+
+    PROBLEM = "0518"
+    data_file = os.path.join(os.path.dirname(__file__), f"leetcode_{PROBLEM}_data.txt")
+
+    # # generate testcases
+    # import sys
+
+    # sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+    # from generic.helper import InputInteger, InputList, generate_testcases
+
+    # arr1 = InputList(n_min_max=(2, 100_000), val_min_max=(0, 10_000))
+    # int1 = InputInteger(val_min_max=(0, 10_000))
+    # vars = generate_testcases(structure=(arr1,), n=1, data_file=data_file, solver=None)
+
+    timing(
+        solution=Solution(),
+        funcs=["change"],
+        data_file=data_file,
+        exclude_data_lines=None,
+        check_result=True,
+    )
