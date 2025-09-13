3059aeea693485a89db9768284fd9a4182f44ac0
unknown <boudewijn_klijn@hotmail.com>
2025-09-13 10:00:24 +0200

    leetcode 3541

---
+++ leetcode/leetcode_3541.py
@@ -0,0 +1,40 @@
+from collections import Counter
+
+
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        vowels = set(list("aieou"))
+        vow, non_vow = 0, 0
+        for k, v in Counter(s).items():
+            if k in vowels:
+                vow = max(vow, v)
+            else:
+                non_vow = max(non_vow, v)
+        return vow + non_vow
+
+
+if __name__ == "__main__":
+    import os
+
+    from timing import timing
+
+    PROBLEM = "TODO"
+    data_file = os.path.join(os.path.dirname(__file__), f"leetcode_{PROBLEM}_data.txt")
+
+    # # generate testcases
+    # import sys
+
+    # sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+    # from generic.helper import InputInteger, InputList, generate_testcases
+
+    # arr1 = InputList(n_min_max=(2, 100_000), val_min_max=(0, 10_000))
+    # int1 = InputInteger(val_min_max=(0, 10_000))
+    # vars = generate_testcases(structure=(arr1,), n=1, data_file=data_file, solver=None)
+
+    timing(
+        solution=Solution(),
+        funcs=["TODO"],
+        data_file=data_file,
+        exclude_data_lines=None,
+        check_result=True,
+    )
