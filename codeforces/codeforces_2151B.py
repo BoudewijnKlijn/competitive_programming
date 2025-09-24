6abda416399177fd4d05c694e57f78387fc321af
unknown <boudewijn_klijn@hotmail.com>
2025-09-24 15:09:12 +0200

    codeforces 2151 B

--- codeforces/codeforces_2151B.py
+++ codeforces/codeforces_2151B.py
@@ -6,27 +6,33 @@ from collections import Counter, defaultdict, deque
 
 
 def solve():
+    """Only the coloring of the final cell _might_ change what the next player does.
+    So, consider the next player being on the position of the player before him, before its last move.
+    Then execute the second last and last command.
+    """
     n, m = list(map(int, input().split()))
-    s = input()
+    commands = input()
     black_squares = set(map(int, input().split()))
 
-    # print(n, m, s, black_squares)
     positions = [None] + [1] * n
+    pos = 1
+    prev_command = None
+    for player in range(1, n + 1):
+        # correction for if previous last command is B
+        # the next (this) player would move to another cell.
+        if prev_command == "B":
+            while pos in black_squares:
+                pos += 1
 
-    for i, command in enumerate(s, start=1):
-        # execute command for all players
-        if command == "A":
-            for player in range(i, n + 1):
-                positions[player] += 1
-            black_squares.add(positions[i])
-        else:
-            min_first_white = positions[i] + 1
-            for player in range(i, n + 1):
-                while min_first_white in black_squares:
-                    min_first_white += 1
-                positions[player] = min_first_white
-                if player == i:
-                    black_squares.add(positions[i])
+        # now execute the extra command for the current player.
+        command = commands[player - 1]
+        pos += 1
+        if command == "B":
+            while pos in black_squares:
+                pos += 1
+        positions[player] = pos
+        black_squares.add(pos)
+        prev_command = command
 
     print(len(black_squares))
     print(*sorted(black_squares))
