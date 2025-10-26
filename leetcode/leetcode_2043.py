8d3f0b04383a3f0014859ac85bc5cb0d27c8f6a2
unknown <boudewijn_klijn@hotmail.com>
2025-10-26 15:04:14 +0100

    leetcode 2043

---
+++ leetcode/leetcode_2043.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Bank:
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+
+    def is_valid_account(self, account):
+        return account - 1 < len(self.balance)
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if self.is_valid_account(account1) and self.is_valid_account(account2):
+            if self.balance[account1 - 1] >= money:
+                self.balance[account1 - 1] -= money
+                self.balance[account2 - 1] += money
+                return True
+        return False
+
+    def deposit(self, account: int, money: int) -> bool:
+        if self.is_valid_account(account):
+            self.balance[account - 1] += money
+            return True
+        return False
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if self.is_valid_account(account):
+            if self.balance[account - 1] >= money:
+                self.balance[account - 1] -= money
+                return True
+        return False
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
